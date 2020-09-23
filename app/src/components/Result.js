bac6ae81e2273c0c581cca67147d0633c6116c7b
Anh Tran <anhtnt@elightup.com>
2020-09-23 11:06:34 +0700

    Use WP CodeMirror to show code

--- app/src/components/Result.js
+++ app/src/components/Result.js
@@ -1,22 +1,24 @@
-import React, { useContext, useState } from 'react';
-import Highlight from 'react-highlight';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
 import Clipboard from 'react-clipboard.js';
 import { Context } from '../context/GeneratorContext';
+const { useState, useContext } = wp.element;
+const { __ } = wp.i18n;
 
 const ResultCode = () => {
-	const state = useContext(Context);
+	const state = useContext( Context );
 	const { data } = state.state;
-	const [copied, setCopied] = useState(false);
-	const copy = () => {
-		setCopied(true);
-		setTimeout(() => setCopied(false), 1000);
+	const [ copied, setCopied ] = useState( false );
+	const changeText = () => {
+		setCopied( true );
+		setTimeout( () => setCopied( false ), 1000 );
 	}
 
 	return data && (
 		<div className="og-result">
+			<p>{ __( 'Copy the code and paste into your theme\'s functions.php file.', 'meta-box-builder' ) }</p>
 			<div className="og-result__body">
-				<Highlight language='php'>{data}</Highlight>
-				<Clipboard title="Click to copy the code" button-class="button" data-clipboard-text={data} onSuccess={copy}>{copied ? 'Copied' : 'Copy'}</Clipboard>
+				<CodeMirror value={ data } options={ { mode: 'php', lineNumbers: true } }/>
+				<Clipboard title="Click to copy the code" button-class="button" data-clipboard-text={ data } onSuccess={ changeText }>{ copied ? __( 'Copied', 'meta-box-builder' ) : __( 'Copy', 'meta-box-builder' ) }</Clipboard>
 			</div>
 		</div>
 	);
