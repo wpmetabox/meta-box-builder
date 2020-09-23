18bacf198b73143f5de7968757e0adbb6f4d818d
Anh Tran <anhtnt@elightup.com>
2020-09-23 11:40:37 +0700

    Remove CSS/HTML from the app

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack')
 
 // https://www.cssigniter.com/how-to-use-external-react-components-in-your-gutenberg-blocks/
@@ -51,17 +49,6 @@ module.exports = {
     plugins: [
         new webpack.optimize.LimitChunkCountPlugin({
             maxChunks: 1
-          }),
-        new HtmlWebPackPlugin({
-            hash: true,
-            filename: "index.html",  //target html
-            template: "./public/index.html" //source html
-        }),
-        new ExtractTextPlugin({ filename: 'css/style.css' }),
-        new MiniCssExtractPlugin({
-          // Options similar to the same options in webpackOptions.output
-          // both options are optional
-          filename: "static/css/bundle.min.css",
         }),
     ]
 }
