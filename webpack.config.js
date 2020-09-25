2f6a61c7c9259564a780f94c5d438b6b405777a8
Anh Tran <anhtnt@elightup.com>
2020-09-25 16:14:12 +0700

    Move config to root folder

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require( "path" );
+const webpack = require( 'webpack' );
+
+// https://www.cssigniter.com/how-to-use-external-react-components-in-your-gutenberg-blocks/
+const externals = {
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+    'codemirror': 'wp.CodeMirror',
+    'clipboard': 'ClipboardJS',
+};
+
+module.exports = {
+    entry: "./app/src/index.js",
+    output: {
+        path: path.join( __dirname, "assets/js" ),
+        filename: "app.js"
+    },
+    externals,
+    module: {
+        rules: [ {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [ '@babel/preset-react', '@babel/preset-env' ],
+                    plugins: [ '@babel/plugin-transform-runtime' ]
+                }
+            }
+        } ]
+    },
+    plugins: [
+        new webpack.optimize.LimitChunkCountPlugin( {
+            maxChunks: 1
+        } ),
+    ]
+};
