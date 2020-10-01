2d15fc06496003bb1ab31447135475d2da604df0
ADMINPC\Admin <nghiait157@gmail.com>
2020-10-01 22:58:50 +0700

    change to use react tabs

--- app/components/MainTabs.js
+++ app/components/MainTabs.js
@@ -5,61 +5,54 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { actions, formatParams } from '../context/GeneratorContext';
 import Result from './Result';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
-const { TabPanel } = wp.components;
+
 const { __ } = wp.i18n;
 const INPUT_FORM_DATA = 'post_content';
+const SUBMIT_FORM_BUTTON = 'submit-form';
 
 const MainTabs = () => {
-  const { handleSubmit, register, control, watch } = useForm();
+  const { handleSubmit, register, control } = useForm();
   const methods = useForm();
-  const onSubmit = data => actions.generatePHPCode(data);
+  const onSubmit = data => {
+    actions.generatePHPCode(data)
+  };
 
   const onPublish = data => {
     const inputData = document.getElementById(INPUT_FORM_DATA)
     inputData.value = JSON.stringify(formatParams(data))
   }
 
-  const tabs = [
-    {
-      name: 'fields',
-      title: __( 'Fields', 'meta-box-builder' ),
-      forceLayout:true
-    },
-    {
-      name: 'settings',
-      title: __( 'Settings', 'meta-box-builder' ),
-    },
-    {
-      name: 'code',
-      title: __( 'Code', 'meta-box-builder' ),
+  const onSelect = (index) => {
+    const isGetCodeTab = index === 2
+    if(isGetCodeTab){
+      document.getElementById(SUBMIT_FORM_BUTTON).click()
     }
-  ];
-  const panels = {
-    fields: (
-      <DndProvider backend={HTML5Backend}>
-        <FieldsTab watch={watch} />
-      </DndProvider>
-    ),
-    settings: <SettingsTab register={register} />,
-    code: <Result />
-  }
-
-  const onSelect = tab => {
-    if ( tab !== 'code' ) {
-      return;
-    }
-    const submitButton = document.getElementById('submit-form')
-    submitButton.click()
-    // TODO: get data in JSON format.
-    // handleSubmit( onSubmit );
   }
 
   return (
     <FormProvider {...methods} register={register} control={control}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TabPanel initialTabName='fields' className="mbb-tabs" tabs={ tabs } onSelect={ onSelect }>{ tab => panels[tab.name] }</TabPanel>
-        <button type="submit" style={{display: 'none'}} id="submit-form" />
+      <form onSubmit={handleSubmit(onSubmit)} id='myForm'>
+        <Tabs forceRenderTabPanel={true}  onSelect={onSelect}>
+          <TabList>
+            <Tab>{__('Fields', 'meta-box-builder')}</Tab>
+            <Tab>{__('Settings', 'meta-box-builder')}</Tab>
+            <Tab>{__('Code', 'meta-box-builder')}</Tab>
+          </TabList>
+          <TabPanel>
+            <DndProvider backend={HTML5Backend}>
+              <FieldsTab />
+            </DndProvider>
+          </TabPanel>
+          <TabPanel>
+            <SettingsTab register={register} />
+          </TabPanel>
+          <TabPanel>
+            <Result />
+          </TabPanel>
+        </Tabs>
+        <button type="submit" style={{display: 'none'}} id={SUBMIT_FORM_BUTTON} />
       </form>
       <input type="hidden" id={INPUT_FORM_DATA} />
       <button onClick={handleSubmit(onPublish)}>Publish</button>
