ba17e9272a85eded11a006a480ff287024e902cc
Anh Tran <anhtnt@elightup.com>
2020-10-02 10:00:35 +0700

    Switch to react-tabs for fields

--- app/components/Tabs/FieldsTab/FieldSelected.js
+++ app/components/Tabs/FieldsTab/FieldSelected.js
@@ -5,95 +5,92 @@ import { ucfirst } from '../../../utility/functions';
 import { DragSource } from 'react-dnd';
 import Types from './Types';
 import { cardSource, collect, copyItem, deleteItem } from '../../../utility/updateSelectedList';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const { useState, memo } = wp.element;
-const { TabPanel } = wp.components;
 const { __ } = wp.i18n;
 
-const FieldSelected = (props) => {
+const FieldSelected = ( props ) => {
   const { connectDragSource } = props;
   const type = props.data.general.type;
   const index = props.id;
-  const [expanded, setExpanded] = useState(false);
-  const toggleSettings = () => setExpanded(!expanded);
-  if ('divider' === type) {
+  const [ expanded, setExpanded ] = useState( false );
+  const toggleSettings = () => setExpanded( !expanded );
+  if ( 'divider' === type ) {
     return connectDragSource(
-      <div className={`og-item og-item--${type} og-collapsible${expanded ? ' og-collapsible--expanded' : ''}`}>
-        <input ref={props.register} type="hidden" name={`fields-${index}-type`} defaultValue={type} />
+      <div className={ `og-item og-item--${ type } og-collapsible${ expanded ? ' og-collapsible--expanded' : '' }` }>
+        <input ref={ props.register } type="hidden" name={ `fields-${ index }-type` } defaultValue={ type } />
         <Header
-          type={type}
-          index={index}
-          name={props.data.general.name}
-          expanded={expanded}
-          copyItem={props.copyItem}
-          removeItem={props.removeItem}
-          toggleSettings={toggleSettings}
+          type={ type }
+          index={ index }
+          name={ props.data.general.name }
+          expanded={ expanded }
+          copyItem={ props.copyItem }
+          removeItem={ props.removeItem }
+          toggleSettings={ toggleSettings }
         />
         <div className="og-item__body og-collapsible__body">
-          <GeneralContent type={type} index={index} fieldData={props.data.general} />
+          <GeneralContent type={ type } index={ index } fieldData={ props.data.general } />
         </div>
       </div>
     );
   }
-  const tabs = [
-    {
-      name: 'general',
-      title: __( 'General', 'meta-box-builder' ),
-    },
-    {
-      name: 'advanced',
-      title: __( 'Advanced', 'meta-box-builder' ),
-    },
-  ];
-  const panels = {
-    general: <GeneralContent type={type} index={index} fieldData={props.data.general} />,
-    advanced: <AdvancedContent type={type} index={index} data={props.data.advanced} />
-  };
 
   return connectDragSource(
-    <div className={`og-item og-item--${type} og-collapsible${expanded ? ' og-collapsible--expanded' : ''}`}>
+    <div className={ `og-item og-item--${ type } og-collapsible${ expanded ? ' og-collapsible--expanded' : '' }` }>
       <li className="d" id="leaf">
-        <input ref={props.register} type="hidden" name={`fields-${index}-type`} defaultValue={type} />
+        <input ref={ props.register } type="hidden" name={ `fields-${ index }-type` } defaultValue={ type } />
         <Header
-          type={type}
-          index={index}
-          name={props.data.general.name}
-          expanded={expanded}
-          copyItem={props.copyItem}
-          removeItem={props.removeItem}
-          toggleSettings={toggleSettings}
-          changeSelectedList={props.changeSelectedList}
-          parent={props.parent}
-          indexVal={props.indexVal}
+          type={ type }
+          index={ index }
+          name={ props.data.general.name }
+          expanded={ expanded }
+          copyItem={ props.copyItem }
+          removeItem={ props.removeItem }
+          toggleSettings={ toggleSettings }
+          changeSelectedList={ props.changeSelectedList }
+          parent={ props.parent }
+          indexVal={ props.indexVal }
         />
-        <TabPanel className="og-item__body og-collapsible__body" tabs={ tabs }>{ tab => panels[tab.name] }</TabPanel>
+        <Tabs className="og-item__body og-collapsible__body">
+          <TabList>
+            <Tab>{ __( 'General', 'meta-box-builder' ) }</Tab>
+            <Tab>{ __( 'Advanced', 'meta-box-builder' ) }</Tab>
+          </TabList>
+          <TabPanel>
+            <GeneralContent type={ type } index={ index } fieldData={ props.data.general } />
+          </TabPanel>
+          <TabPanel>
+            <AdvancedContent type={ type } index={ index } data={ props.data.advanced } />
+          </TabPanel>
+        </Tabs>
       </li>
     </div>
   );
 };
-const Header = (props) => {
-  const duplicate = (e) => {
+const Header = ( props ) => {
+  const duplicate = ( e ) => {
     e.stopPropagation();
-    const newSelectedList = copyItem(props.index, props.parent, props.indexVal)
-    props.changeSelectedList(newSelectedList)
+    const newSelectedList = copyItem( props.index, props.parent, props.indexVal );
+    props.changeSelectedList( newSelectedList );
   };
-  const remove = (e) => {
+  const remove = ( e ) => {
     e.stopPropagation();
-    const newSelectedList = deleteItem(props.index, props.parent, props.indexVal)
-    props.changeSelectedList(newSelectedList)
+    const newSelectedList = deleteItem( props.index, props.parent, props.indexVal );
+    props.changeSelectedList( newSelectedList );
   };
 
   return (
-    <div className="og-item__header og-collapsible__header" onClick={props.toggleSettings}>
-      <div className="og-item__title" id={`og-item__title__${props.index}`}>{props.name || ucfirst(props.type)}</div>
+    <div className="og-item__header og-collapsible__header" onClick={ props.toggleSettings }>
+      <div className="og-item__title" id={ `og-item__title__${ props.index }` }>{ props.name || ucfirst( props.type ) }</div>
       <div className="og-item__actions">
-        <span className="og-item__type">{props.type}</span>
-        <span className="og-item__action og-item__action--remove" title="Remove" onClick={remove}>{trashIcon}</span>
-        <span className="og-item__action og-item__action--duplicate" title="Duplicate" onClick={duplicate}>{copyIcon}</span>
-        <span className="og-item__action og-item__action--toggle" title="Toggle Settings">{props.expanded ? arrowUpIcon : arrowDownIcon}</span>
+        <span className="og-item__type">{ props.type }</span>
+        <span className="og-item__action og-item__action--remove" title="Remove" onClick={ remove }>{ trashIcon }</span>
+        <span className="og-item__action og-item__action--duplicate" title="Duplicate" onClick={ duplicate }>{ copyIcon }</span>
+        <span className="og-item__action og-item__action--toggle" title="Toggle Settings">{ props.expanded ? arrowUpIcon : arrowDownIcon }</span>
       </div>
     </div>
   );
 };
 
-export default memo(DragSource(Types.CARD, cardSource, collect)(FieldSelected), (prevProps, nextProps) => prevProps.id === nextProps.id);
+export default memo( DragSource( Types.CARD, cardSource, collect )( FieldSelected ), ( prevProps, nextProps ) => prevProps.id === nextProps.id );
