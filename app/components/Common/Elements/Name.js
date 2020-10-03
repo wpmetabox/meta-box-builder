8a474ae554a87eaef42b47fcf4ee276a79e2e60f
Dang Nghia <dangnghia@Dangs-MacBook-Pro.local>
2020-10-03 15:04:24 +0700

    fix label

--- app/components/Common/Elements/Name.js
+++ app/components/Common/Elements/Name.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 import DivRow from '../DivRow';
 
-const Name = ({ name, defaultValue, register, ...rest }) => {
-  const changeFieldLabel = e => document.getElementById(`og-item__title__${rest.data.id}`).textContent = e.target.value;
+const Name = ( { name, defaultValue, ...rest } ) => {
+  const changeFieldLabel = e => document.getElementById( `og-item__title__${ rest.data.id }` ).textContent = e.target.value;
+  const { register } = useFormContext();
 
   return (
-    <DivRow htmlFor={name} {...rest} label="Label" tooltip="Optional field label. If empty, the field input is 100% width.">
-      <input type="text" id={name} name={name} ref={register} defaultValue={defaultValue} onChange={changeFieldLabel} />
+    <DivRow htmlFor={ name } { ...rest } label="Label" tooltip="Optional field label. If empty, the field input is 100% width.">
+      <input type="text" id={ name } name={ name } ref={ register } defaultValue={ defaultValue } onChange={ changeFieldLabel } />
     </DivRow>
-  )
-}
+  );
+};
 export default Name;
