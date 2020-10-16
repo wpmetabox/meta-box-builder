4baf98e85deb96df1edfd2e7dfa652ff2c669797
Anh Tran <anhtnt@elightup.com>
2020-10-16 14:21:01 +0700

    Update style for custom attributes

--- app/components/Common/Elements/Attributes.js
+++ app/components/Common/Elements/Attributes.js
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
+import DivRow from '../DivRow';
 import AdvancedAdditionalItem from './AdvancedAdditionalItem.js';
 
-const Attributes = props => {
-  const [list, setList] = useState([]);
-  const removeItem = id => {
-    setList(prevList => prevList.filter(item => item.unixId !== id));
-  }
+const { __ } = wp.i18n;
+const { useState } = wp.element;
 
-  return (
-    <div className="og-attributes">
-      <h4><a href="https://docs.metabox.io/extensions/meta-box-builder/#custom-attributes" target="_blank" rel="noreferrer noopener">Custom attributes</a></h4>
-      {
-        list.map((item, index) => (
-          <AdvancedAdditionalItem data={item} key={item.unixId} index={index} removeItem={removeItem} name={`fields-${props.index}`} type='attributes' />
-        ))
-      }
-      <button type="button" className="button" onClick={() => setList(prevList => prevList.concat({ key: '', label: '', unixId: Math.random().toString(36).slice(-5) }))}>+ Add Attribute</button>
-    </div>
-  )
-}
+const Attributes = props => {
+	const [ list, setList ] = useState( [] );
+	const removeItem = id => {
+		setList( prevList => prevList.filter( item => item.unixId !== id ) );
+	};
+
+	return (
+		<DivRow label={ `<a href="https://docs.metabox.io/extensions/meta-box-builder/#custom-attributes" target="_blank" rel="noreferrer noopener">${ __( 'Custom attributes', 'meta-box-builder' ) }</a>`}>
+			{
+				list.map( ( item, index ) => (
+					<AdvancedAdditionalItem data={ item } key={ item.unixId } index={ index } removeItem={ removeItem } name={ `fields-${ props.index }` } type='attributes' />
+				) )
+			}
+			<button type="button" className="button" onClick={ () => setList( prevList => prevList.concat( { key: '', label: '', unixId: Math.random().toString( 36 ).slice( -5 ) } ) ) }>{ __( '+ Add Attribute', 'meta-box-builder' ) }</button>
+		</DivRow>
+	);
+};
 
 export default Attributes;
