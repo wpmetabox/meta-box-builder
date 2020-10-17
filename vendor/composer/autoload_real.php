346f068e2b74248e3525a7eba559e9e352a180d6
Anh Tran <anhtnt@elightup.com>
2020-10-17 23:28:39 +0700

    Update mbb-parser

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInitb514f273ab9a36d3fea19808c7c51d62
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
