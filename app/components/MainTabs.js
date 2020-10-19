5b502bf408438723cbdb9f4fbb84bc3ab3c3390c
Anh Tran <anhtnt@elightup.com>
2020-10-19 10:09:12 +0700

    Use FormProvider instead of FormContext

--- app/components/MainTabs.js
+++ app/components/MainTabs.js
@@ -1,4 +1,4 @@
-import { useForm, FormContext } from "react-hook-form";
+import { useForm, FormProvider } from 'react-hook-form';
 import SettingsTab from './Tabs/SettingsTab';
 import FieldsTab from './Tabs/FieldsTab';
 import { DndProvider } from 'react-dnd';
@@ -13,59 +13,59 @@ const { __ } = wp.i18n;
 const SUBMIT_FORM_BUTTON = 'submit-form';
 
 const MainTabs = () => {
-  const { handleSubmit, register, control } = useForm();
-  const methods = useForm();
-  const onSubmit = data => {
-    actions.generatePHPCode( data );
-  };
+	const { handleSubmit, register, control } = useForm();
+	const methods = useForm();
+	const onSubmit = data => {
+		actions.generatePHPCode( data );
+	};
 
-  const onPublish = data => {
-    const inputData = document.getElementById( 'post_content' );
-    inputData.value = JSON.stringify( formatParams( data ) );
-  };
+	const onPublish = data => {
+		const inputData = document.getElementById( 'post_content' );
+		inputData.value = JSON.stringify( formatParams( data ) );
+	};
 
-  const onSelect = ( index ) => {
-    const isGetCodeTab = index === 2;
-    if ( isGetCodeTab ) {
-      document.getElementById( SUBMIT_FORM_BUTTON ).click();
-    }
-  };
+	const onSelect = ( index ) => {
+		const isGetCodeTab = index === 2;
+		if ( isGetCodeTab ) {
+			document.getElementById( SUBMIT_FORM_BUTTON ).click();
+		}
+	};
 
-  useEffect( () => {
-    document.querySelector( '#publish' ).addEventListener( 'click', () => {
-      document.getElementById( 'btn-on-publish' ).click();
-    } );
-  }, [] );
+	useEffect( () => {
+		document.querySelector( '#publish' ).addEventListener( 'click', () => {
+			document.getElementById( 'btn-on-publish' ).click();
+		} );
+	}, [] );
 
-  const fields = MbbApp.settings ? MbbApp.settings.fields : [];
+	const fields = MbbApp.settings ? MbbApp.settings.fields : [];
 
-  return (
-    <FormContext { ...methods } register={ register } control={ control }>
-      <form onSubmit={ handleSubmit( onSubmit ) } id='myForm'>
-        <Tabs forceRenderTabPanel={ true } onSelect={ onSelect }>
-          <TabList>
-            <Tab>{ __( 'Fields', 'meta-box-builder' ) }</Tab>
-            <Tab>{ __( 'Settings', 'meta-box-builder' ) }</Tab>
-            <Tab>{ __( 'Code', 'meta-box-builder' ) }</Tab>
-          </TabList>
-          <TabPanel>
-            <DndProvider backend={ HTML5Backend }>
-              <FieldsTab fields={ parseFields( fields ) } />
-            </DndProvider>
-          </TabPanel>
-          <TabPanel>
-            <SettingsTab register={ register } />
-          </TabPanel>
-          <TabPanel>
-            <Result />
-          </TabPanel>
-        </Tabs>
-        <button type="submit" style={ { display: 'none' } } id={ SUBMIT_FORM_BUTTON } />
-      </form>
-      <input type="hidden" id="post_content" name="post_content" />
-      <button style={ { display: 'none' } } id="btn-on-publish" onClick={ handleSubmit( onPublish ) }>Publish</button>
-    </FormContext>
-  );
+	return (
+		<FormProvider { ...methods } register={ register } control={ control }>
+			<form onSubmit={ handleSubmit( onSubmit ) } id='myForm'>
+				<Tabs forceRenderTabPanel={ true } onSelect={ onSelect }>
+					<TabList>
+						<Tab>{ __( 'Fields', 'meta-box-builder' ) }</Tab>
+						<Tab>{ __( 'Settings', 'meta-box-builder' ) }</Tab>
+						<Tab>{ __( 'Code', 'meta-box-builder' ) }</Tab>
+					</TabList>
+					<TabPanel>
+						<DndProvider backend={ HTML5Backend }>
+							<FieldsTab fields={ parseFields( fields ) } />
+						</DndProvider>
+					</TabPanel>
+					<TabPanel>
+						<SettingsTab register={ register } />
+					</TabPanel>
+					<TabPanel>
+						<Result />
+					</TabPanel>
+				</Tabs>
+				<button type="submit" style={ { display: 'none' } } id={ SUBMIT_FORM_BUTTON } />
+			</form>
+			<input type="hidden" id="post_content" name="post_content" />
+			<button style={ { display: 'none' } } id="btn-on-publish" onClick={ handleSubmit( onPublish ) }>Publish</button>
+		</FormProvider>
+	);
 };
 
 export default MainTabs;
