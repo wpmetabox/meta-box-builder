18e283553ef1c2af66cc5b7479f5cb8cc05e7de1
Anh Tran <anhtnt@elightup.com>
2020-10-22 16:11:00 +0700

    Replace react-clipboard with Gutenberg component

--- app/components/Result.js
+++ app/components/Result.js
@@ -1,24 +1,28 @@
 import { UnControlled as CodeMirror } from 'react-codemirror2';
-import Clipboard from 'react-clipboard.js';
 import { Context } from '../context/Generator/GeneratorContext';
-const { useState, useContext } = wp.element;
+const { useContext } = wp.element;
 const { __ } = wp.i18n;
+const { ClipboardButton } = wp.components;
+const { withState }  = wp.compose;
 
 const ResultCode = () => {
 	const state = useContext( Context );
 	const { data } = state.state;
-	const [ copied, setCopied ] = useState( false );
-	const changeText = () => {
-		setCopied( true );
-		setTimeout( () => setCopied( false ), 1000 );
-	}
+
+	const Button = withState( {
+		hasCopied: false,
+	} )( ( { hasCopied, setState } ) => (
+		<ClipboardButton className="button" text={ data } onCopy={ () => setState( { hasCopied: true } ) } onFinishCopy={ () => setState( { hasCopied: false } ) }>
+			{ hasCopied ? __( 'Copied!', 'meta-box-builder' ) : __( 'Copy', 'meta-box-builder' ) }
+		</ClipboardButton>
+	) );
 
 	return data && (
 		<div className="og-result">
 			<p>{ __( 'Copy the code and paste into your theme\'s functions.php file.', 'meta-box-builder' ) }</p>
 			<div className="og-result__body">
 				<CodeMirror value={ data } options={ { mode: 'php', lineNumbers: true } }/>
-				<Clipboard title="Click to copy the code" button-class="button" data-clipboard-text={ data } onSuccess={ changeText }>{ copied ? __( 'Copied', 'meta-box-builder' ) : __( 'Copy', 'meta-box-builder' ) }</Clipboard>
+				<Button />
 			</div>
 		</div>
 	);
