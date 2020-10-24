d4c941af2c12412fbd3bec7442518340545e539b
Anh Tran <anhtnt@elightup.com>
2020-10-24 13:28:49 +0700

    Refactor the Inserter component

--- app/components/Common/Inserter.js
+++ app/components/Common/Inserter.js
@@ -1,38 +1,77 @@
 import { request } from '../../utility/functions';
-import { FieldMenu } from './FieldMenu';
-import { SearchResultList } from './SearchResultList';
 
-const { useState, useEffect } = wp.element;
-const { Button, Dropdown } = wp.components;
+const { useState, useEffect, Fragment } = wp.element;
+const { Dropdown } = wp.components;
 const { __ } = wp.i18n;
 
-export const Inserter = ( props ) => {
-    const [ searchParam, setSearchParam ] = useState( '' );
+export const Inserter = ( { addItem } ) => {
 	const [ fieldTypes, setFieldTypes ] = useState( {} );
+	const [ searchParam, setSearchParam ] = useState( '' );
+	const [ closeCallback, setCloseCallback ] = useState( () => { } );
 
 	useEffect( () => {
 		request( 'field-types' ).then( data => setFieldTypes( data ) );
 	}, [] );
 
-    return (
-        <Dropdown
-            className="og-inserter"
-            position="top left"
-            renderToggle={ ( { isOpen, onToggle } ) => (
-                <Button isPrimary onClick={ onToggle } aria-expanded={ isOpen }>{ __( 'Add Field', 'meta-box-builder' ) }</Button>
-            ) }
-            renderContent={ () => (
-                <>
-                    <div className="og-inserter__search">
-                        <input type="search" placeholder={ __( 'Search for a field type', 'meta-box-builder' ) } onChange={ e => setSearchParam( e.target.value ) } />
-                    </div>
-                    {
-                        searchParam
-                            ? <SearchResultList fieldTypes={ fieldTypes } onSelectField={ props.addItem } searchParam={ searchParam } />
-                            : <FieldMenu fieldTypes={ fieldTypes } onSelectField={ props.addItem } />
-                    }
-                </>
-            ) }
-        />
-    );
+	const search = e => setSearchParam( e.target.value );
+	const open = ( toggle, close ) => {
+		toggle();
+		setCloseCallback( () => close );
+	};
+	const insert = e => {
+		addItem( e.target.dataset.type );
+		closeCallback();
+		setSearchParam( '' );
+	};
+
+	const Category = ( { title, items } ) => {
+		let result = [];
+		Object.entries( items ).forEach( ( [ type, label ] ) => {
+			if ( label.toLowerCase().includes( searchParam.toLowerCase() ) ) {
+				result.push( { type, label } );
+			}
+		} );
+
+		if ( !result.length ) {
+			return null;
+		}
+
+		return (
+			<>
+				{ title && <div className="og-inserter__title">{ title }</div> }
+				<div className="og-inserter__content">
+					{
+						result.map( ( { type, label } ) =>
+							<button type="button" className="button" key={ type } data-type={ type } onClick={ insert }>{ label }</button>
+						)
+					}
+				</div>
+			</>
+		);
+	};
+
+	return (
+		<Dropdown
+			className="og-inserter"
+			renderToggle={ ( { onToggle, onClose } ) => (
+				<button type="button" className="button button-primary" onClick={ () => open( onToggle, onClose ) }>{ __( '+ Add Field', 'meta-box-builder' ) }</button>
+			) }
+			renderContent={ () => (
+				<>
+					<div className="og-inserter__search">
+						<input type="search" placeholder={ __( 'Search for a field type', 'meta-box-builder' ) } onChange={ search } />
+					</div>
+					{
+						Object.keys( fieldTypes ).length
+							? Object.entries( fieldTypes ).map( ( [ title, items ] ) =>
+								<Fragment key={ title }>
+									<Category title={ title } items={ items } />
+								</Fragment>
+							)
+							: <p>{ __( 'Fetching field types, please wait...', 'meta-box-builder' ) }</p>
+					}
+				</>
+			) }
+		/>
+	);
 };
