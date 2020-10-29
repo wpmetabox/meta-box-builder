6cf8ef422ff5b0ff9894d4a036bc39e6583bfd92
Anh Tran <anhtnt@elightup.com>
2020-10-29 11:49:12 +0700

    Add basic style and handle add/remove rule

--- app/components/Tabs/SettingsTab/IncludeExclude.js
+++ app/components/Tabs/SettingsTab/IncludeExclude.js
@@ -1,27 +1,32 @@
+import { useFormContext } from 'react-hook-form';
 import { uniqid } from '../../../utility/functions';
 import DivRow from '../../Common/DivRow';
 const { useState } = wp.element;
+const { Dashicon } = wp.components;
 const { __ } = wp.i18n;
 
 export const IncludeExclude = () => {
 	const [ rules, setRules ] = useState( [] );
 
-	const addRule = () => {
-		setRules( prevRules => {
-			let newRules = [ ...prevRules ];
-			newRules.push( { name: '', value: '', id: uniqid() } );
-			return newRules;
-		} );
-	};
+	const addRule = () => setRules( prevRules => prevRules.concat( { name: '', value: '', id: uniqid() } ) );
+	const removeRule = id => setRules( prevRules => prevRules.filter( rule => rule.id !== id ) );
 
 	return (
 		<DivRow
+			className="og-include-exclude"
 			label={ `<a href="https://metabox.io/plugins/meta-box-include-exclude/" target="_blank" rel="noopener norefferer">${ __( 'Advanced rules', 'meta-box-builder' ) }</a>` }
 			tooltip={ __( 'More controls on where to show this field group.', 'meta-box-builder' ) }
 		>
-			{ rules.length && <Intro /> }
-			{ rules.length && rules.map( ( rule, index ) => <Rule key={ rule.id } rule={ rule } index={ index } /> ) }
-			<AddRule addRule={ addRule } />
+			{ rules.length > 0 && <Intro /> }
+			{
+				rules.map( ( rule, index ) => <Rule
+					key={ rule.id }
+					rule={ rule }
+					index={ index }
+					removeRule={ removeRule }
+				/> )
+			}
+			<button type="button" className="button" onClick={ addRule }>{ __( '+ Add Rule', 'meta-box-builder' ) }</button>
 		</DivRow>
 	);
 };
@@ -30,7 +35,7 @@ const Intro = () => {
 	const { register } = useFormContext();
 
 	return (
-		<div className="og-include-exclude-intro">
+		<div className="og-include-exclude__intro">
 			<select name="include_exclude[type]" ref={ register }>
 				<option value="include">{ __( 'Show', 'meta-box-builder' ) }</option>
 				<option value="exclude">{ __( 'Hide', 'meta-box-builder' ) }</option>
@@ -45,30 +50,31 @@ const Intro = () => {
 	);
 };
 
-const Rule = ( { rule, index } ) => {
+const Rule = ( { rule, index, removeRule } ) => {
 	const { register } = useFormContext();
 	const [ name, setName ] = useState( rule.name );
 	const onChangeName = e => setName( e.target.value );
 
 	return (
-		<select name={ `include_exclude[rules][${ index }][name]` } ref={ register } defaultValue={ name } onChange={ onChangeName }>
-			<option value="ID">{ __( 'Post', 'meta-box-builder' ) }</option>
-			<option value="parent">{ __( 'Parent post', 'meta-box-builder' ) }</option>
-			<option value="template">{ __( 'Page template', 'meta-box-builder' ) }</option>
-			{
-				MbbApp.taxonomies.map( taxonomy => <option key={ taxonomy.slug } value={ taxonomy.slug }>{ taxonomy.name } ({ taxonomy.slug })</option> )
-			}
-			{
-				MbbApp.taxonomies.map( taxonomy => <option key={ taxonomy.slug } value={ `parent_${ taxonomy.slug }` }>{ __( 'Parent', 'meta-box-builder' ) } { taxonomy.name } ({ taxonomy.slug })</option> )
-			}
-			<option value="user_role">{ __( 'User role', 'meta-box-builder' ) }</option>
-			<option value="user_id">{ __( 'User', 'meta-box-builder' ) }</option>
-			<option value="edited_user_role">{ __( 'Edited user role', 'meta-box-builder' ) }</option>
-			<option value="edited_user_id">{ __( 'Edited user', 'meta-box-builder' ) }</option>
-			<option value="is_child">{ __( 'Child post', 'meta-box-builder' ) }</option>
-			<option value="custom">{ __( 'Custom PHP callback', 'meta-box-builder' ) }</option>
-		</select>
+		<div className="og-include-exclude__rule og-attribute">
+			<select name={ `include_exclude[rules][${ index }][name]` } ref={ register } defaultValue={ name } onChange={ onChangeName }>
+				<option value="ID">{ __( 'Post', 'meta-box-builder' ) }</option>
+				<option value="parent">{ __( 'Parent post', 'meta-box-builder' ) }</option>
+				<option value="template">{ __( 'Page template', 'meta-box-builder' ) }</option>
+				{
+					MbbApp.taxonomies.map( taxonomy => <option key={ taxonomy.slug } value={ taxonomy.slug }>{ taxonomy.name } ({ taxonomy.slug })</option> )
+				}
+				{
+					MbbApp.taxonomies.map( taxonomy => <option key={ taxonomy.slug } value={ `parent_${ taxonomy.slug }` }>{ __( 'Parent', 'meta-box-builder' ) } { taxonomy.name } ({ taxonomy.slug })</option> )
+				}
+				<option value="user_role">{ __( 'User role', 'meta-box-builder' ) }</option>
+				<option value="user_id">{ __( 'User', 'meta-box-builder' ) }</option>
+				<option value="edited_user_role">{ __( 'Edited user role', 'meta-box-builder' ) }</option>
+				<option value="edited_user_id">{ __( 'Edited user', 'meta-box-builder' ) }</option>
+				<option value="is_child">{ __( 'Child post', 'meta-box-builder' ) }</option>
+				<option value="custom">{ __( 'Custom PHP callback', 'meta-box-builder' ) }</option>
+			</select>
+			<button type="button" title={ __( 'Remove', 'meta-box-builder' ) } onClick={ () => removeRule( rule.id ) }><Dashicon icon="dismiss" /></button>
+		</div>
 	);
 };
-
-const AddRule = ( { addRule } ) => <button type="button" className="button" onClick={ addRule }>{ __( '+ Add Rule', 'meta-box-builder' ) }</button>;
