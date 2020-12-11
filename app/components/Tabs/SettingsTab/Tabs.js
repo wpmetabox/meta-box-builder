5a66dd1d48affd7cec3d3417dd717afa38dcb3be
Anh Tran <anhtnt@elightup.com>
2020-12-11 11:09:33 +0700

    Settings: save tabs

--- app/components/Tabs/SettingsTab/Tabs.js
+++ app/components/Tabs/SettingsTab/Tabs.js
@@ -1,19 +1,18 @@
+import dotProp from 'dot-prop';
 import Checkbox from '../../Common/Checkbox';
 import Select from '../../Common/Select';
 const { __ } = wp.i18n;
 
-export const Tabs = ( props ) => {
-	return <>
-		<Select
-			name="tab_style"
-			label={ __( 'Tab style', 'meta-box-builder' ) }
-			options={ {
-				default: __( 'Default', 'meta-box-builder' ),
-				box: __( 'Box', 'meta-box-builder' ),
-				left: __( 'Left', 'meta-box-builder' ),
-			} }
-			defaultValue={ props.defaultValues?.tab_style || "default" }
-		/>
-		<Checkbox name="tab_wrapper" label={ __( 'Show meta box wrapper', 'meta-box-builder' ) } defaultValue={ props.defaultValues?.tab_wrapper } />
-	</>;
-};
+export const Tabs = ( { defaultValues } ) => <>
+	<Select
+		name="tab_style"
+		label={ __( 'Tab style', 'meta-box-builder' ) }
+		options={ {
+			default: __( 'Default', 'meta-box-builder' ),
+			box: __( 'Box', 'meta-box-builder' ),
+			left: __( 'Left', 'meta-box-builder' ),
+		} }
+		defaultValue={ dotProp.get( defaultValues, 'tab_style', 'default' ) }
+	/>
+	<Checkbox name="tab_wrapper" label={ __( 'Show meta box wrapper', 'meta-box-builder' ) } defaultValue={ dotProp.get( defaultValues, 'tab_wrapper' ) } />
+</>;
