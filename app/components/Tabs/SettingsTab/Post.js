8a437ebe272b393201d8755abb2477e549f65124
Anh Tran <anhtnt@elightup.com>
2020-12-11 09:04:43 +0700

    Settings: save value for posts

--- app/components/Tabs/SettingsTab/Post.js
+++ app/components/Tabs/SettingsTab/Post.js
@@ -1,3 +1,4 @@
+import dotProp from 'dot-prop';
 import Checkbox from '../../Common/Checkbox';
 import Select from '../../Common/Select';
 const { __ } = wp.i18n;
@@ -19,19 +20,19 @@ export const Post = ( { postTypes, defaultValues } ) => {
 			name="position"
 			label={ __( 'Position', 'meta-box-builder' ) }
 			options={ contextOptions }
-			defaultValue={ defaultValues?.position || "normal" }
+			defaultValue={ dotProp.get( defaultValues, 'position', 'normal' ) }
 		/>
 		<Select
 			name="priority"
 			label={ __( 'Priority', 'meta-box-builder' ) }
 			options={ { high: __( 'High', 'meta-box-builder' ), low: __( 'Low', 'meta-box-builder' ) } }
-			defaultValue={ defaultValues?.priority || "high" }
+			defaultValue={ dotProp.get( defaultValues, 'priority', 'high' ) }
 		/>
 		<Select
 			name="style"
 			label={ __( 'Style', 'meta-box-builder' ) }
 			options={ { default: __( 'Standard (WordPress meta box)', 'meta-box-builder' ), seamless: __( 'Seamless (no meta box)', 'meta-box-builder' ) } }
-			defaultValue={ defaultValues?.style || "high" }
+			defaultValue={ dotProp.get( defaultValues, 'style', 'default' ) }
 		/>
 		{
 			isClassic &&
@@ -39,11 +40,12 @@ export const Post = ( { postTypes, defaultValues } ) => {
 				name="default_hidden"
 				label={ __( 'Hidden by default', 'meta-box-builder' ) }
 				tooltip={ __( 'The meta box is hidden by default and requires users to select the corresponding checkbox in Screen Options to show it', 'meta-box-builder' ) }
+				defaultValue={ dotProp.get( defaultValues, 'default_hidden', false ) }
 			/>
 		}
 		{
 			isClassic &&
-			<Checkbox name="autosave" label={ __( 'Autosave', 'meta-box-builder' ) } />
+			<Checkbox name="autosave" label={ __( 'Autosave', 'meta-box-builder' ) } defaultValue={ dotProp.get( defaultValues, 'autosave', false ) } />
 		}
 	</>;
 };
