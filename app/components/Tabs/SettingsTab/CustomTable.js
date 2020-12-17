d59b390b7cd8333bd6c28d9d31e36b9d676ca4c8
Anh Tran <anhtnt@elightup.com>
2020-12-17 15:03:14 +0700

    Settings: improve custom table

--- app/components/Tabs/SettingsTab/CustomTable.js
+++ app/components/Tabs/SettingsTab/CustomTable.js
@@ -7,31 +7,31 @@ const { useState } = wp.element;
 export const CustomTable = ( { defaultValues } ) => {
 	return <>
 		<Checkbox
-			name="table_enable"
+			name="custom_table[enable]"
 			label={ `<a href="https://metabox.io/plugins/mb-custom-table/" target="_blank" rel="noopener noreferrer">${ __( 'Save data in a custom table', 'meta-box-builder' ) }</a>` }
 			componentId="settings-table_enable"
-			defaultValue={ dotProp.get( defaultValues, 'table_enable' ) }
+			defaultValue={ dotProp.get( defaultValues, 'enable' ) }
 		/>
 		<Input
-			name="table_name"
+			name="custom_table[name]"
 			label={ __( 'Table name', 'meta-box-builder' ) }
 			componentId="settings-table_name"
-			defaultValue={ dotProp.get( defaultValues, 'table_name' ) }
+			defaultValue={ dotProp.get( defaultValues, 'name' ) }
 			dependency="table_enable:true"
 		/>
 		<Checkbox
-			name="table_prefix"
+			name="custom_table[prefix]"
 			label={ __( 'Include table prefix', 'meta-box-builder' ) }
 			componentId="settings-table_prefix"
-			defaultValue={ dotProp.get( defaultValues, 'table_prefix' ) }
+			defaultValue={ dotProp.get( defaultValues, 'prefix' ) }
 			dependency="table_enable:true"
 		/>
 		<Checkbox
-			name="table_create"
+			name="custom_table[create]"
 			label={ __( 'Create table automatically', 'meta-box-builder' ) }
 			tooltip={ __( 'Enable this option will automatically create the table with all columns as TEXT. Create the table manually to set proper column types for a better performance.', 'meta-box-builder' ) }
 			componentId="settings-table_create"
-			defaultValue={ dotProp.get( defaultValues, 'table_create' ) }
+			defaultValue={ dotProp.get( defaultValues, 'create' ) }
 			dependency="table_enable:true"
 		/>
 	</>;
