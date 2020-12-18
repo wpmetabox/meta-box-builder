dcbb405c0bc0295d3c6bf34b1253c28fb4e9db65
Anh Tran <anhtnt@elightup.com>
2020-12-18 14:47:55 +0700

    Remove field

--- app/components/Tabs/FieldsTab.js
+++ app/components/Tabs/FieldsTab.js
@@ -1,3 +1,4 @@
+import dotProp from 'dot-prop';
 import { ucwords, uniqid } from '../../utility/functions';
 import { Inserter } from '../Common/Inserter';
 import Node from './FieldsTab/Node';
@@ -7,16 +8,25 @@ const { __ } = wp.i18n;
 
 const FieldsTab = ( props ) => {
 	const [ fields, setFields ] = useState( props.fields );
-	const addField = type => setFields( prevFields => {
+	const addField = type => setFields( prev => {
 		const id = uniqid();
-		return { ...prevFields, [ id ]: { type, name: ucwords( type ), id } }
-	 } );
+		return { ...prev, [ id ]: { type, name: ucwords( type ), id } };
+	} );
+	const removeField = id => setFields( prev => {
+		let newFields = { ...prev };
+		dotProp.delete( newFields, id );
+		return newFields;
+	} );
 
 	return (
 		<>
 			{ Object.values( fields ).length === 0 && <p className="og-none" dangerouslySetInnerHTML={ { __html: __( 'There are no fields here. Click the <strong>+ Add Field</strong> to add a new field.', 'meta-box-builder' ) } } /> }
 			<div className="og-fields">
-				{ Object.entries( fields ).map( ( [ id, field ] ) => <Node key={ id } id={ id } field={ field } /> ) }
+				{
+					Object.entries( fields ).map( ( [ id, field ] ) =>
+						<Node key={ id } id={ id } field={ field } removeField={ removeField } />
+					)
+				}
 			</div>
 			<Inserter addField={ addField } />
 		</>
