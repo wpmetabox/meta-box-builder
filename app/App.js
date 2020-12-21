97776ff564bae21ea51368f0a343977f3e3c1c20
Anh Tran <anhtnt@elightup.com>
2020-12-21 15:13:09 +0700

    Remove Header & FieldTypes context

--- app/App.js
+++ app/App.js
@@ -1,7 +1,6 @@
 import MainTabs from './components/MainTabs';
 import { Provider as CommonDataProvider } from './context/CommonData/CommonDataContext';
 import { Provider as UpdateConditionalProvider } from './context/ConditionalList/ConditionalContext';
-import { Provider as FieldTypesProvider } from './context/FieldTypes/FieldTypesContext';
 import { Provider as GeneratorProvider } from './context/Generator/GeneratorContext';
 
 const { render } = wp.element;
@@ -10,9 +9,7 @@ const App = () => (
   <CommonDataProvider>
     <GeneratorProvider>
       <UpdateConditionalProvider>
-        <FieldTypesProvider>
-          <MainTabs />
-        </FieldTypesProvider>
+        <MainTabs />
       </UpdateConditionalProvider>
     </GeneratorProvider>
   </CommonDataProvider>
--- app/context/FieldTypes/FieldTypesReducer.js
+++
@@ -1,6 +0,0 @@
-const fieldTypesReducer = (state, action) => {
-    const type = action.type;
-    return { ...state,[type]: action.payload};
-  };
-  
-  export default fieldTypesReducer;
