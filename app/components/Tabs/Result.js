ff9dfc16c222626910ca8ee6d346dee4c0e232ee
Anh Tran <anhtnt@elightup.com>
2020-12-22 13:50:04 +0700

    Add component ID for the result tab

--- app/components/Tabs/Result.js
+++ app/components/Tabs/Result.js
@@ -35,17 +35,20 @@ const ResultCode = () => {
 			name="prefix"
 			label={ __( 'Field ID prefix', 'meta-box-builder' ) }
 			tooltip={ __( 'Auto add a prefix to all field IDs to keep them separated from other field groups or other plugins. Leave empty to ignore this or use underscore (_) to make the fields hidden.', 'meta-box-builder' ) }
+			componentId="prefix"
 		/>
 		<Input
 			name="text_domain"
 			label={ __( 'Text domain', 'meta-box-builder' ) }
 			tooltip={ __( 'Required for multilingual website. Used in the exported code only.', 'meta-box-builder' ) }
 			defaultValue="your-text-domain"
+			componentId="text-domain"
 		/>
 		<Input
 			name="function_name"
 			label={ __( 'Function name', 'meta-box-builder' ) }
 			defaultValue="your_prefix_register_meta_boxes"
+			componentId="function-name"
 		/>
 		<button type="button" className="button" onClick={ generate }>{ __( 'Generate', 'meta-box-builder' ) }</button>
 		{ isGenerating && <p>{ __( 'Generating code, please wait...', 'meta-box-builder' ) }</p> }
