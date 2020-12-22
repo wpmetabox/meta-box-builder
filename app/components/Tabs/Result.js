02d9bd1165e42010039e17ebb968872ab3883c2a
Anh Tran <anhtnt@elightup.com>
2020-12-22 10:57:32 +0700

    Add encoders to generator

--- app/components/Tabs/Result.js
+++ app/components/Tabs/Result.js
@@ -35,13 +35,17 @@ const ResultCode = () => {
 			name="prefix"
 			label={ __( 'Field ID prefix', 'meta-box-builder' ) }
 			tooltip={ __( 'Auto add a prefix to all field IDs to keep them separated from other field groups or other plugins. Leave empty to ignore this or use underscore (_) to make the fields hidden.', 'meta-box-builder' ) }
-			defaultValue="your_prefix_"
 		/>
 		<Input
 			name="text_domain"
 			label={ __( 'Text domain', 'meta-box-builder' ) }
 			tooltip={ __( 'Required for multilingual website. Used in the exported code only.', 'meta-box-builder' ) }
 			defaultValue="your-text-domain"
+		/>
+		<Input
+			name="function_name"
+			label={ __( 'Function name', 'meta-box-builder' ) }
+			defaultValue="your_prefix_register_meta_boxes"
 		/>
 		<button type="button" className="button" onClick={ generate }>{ __( 'Generate', 'meta-box-builder' ) }</button>
 		{ isGenerating && <p>{ __( 'Generating code, please wait...', 'meta-box-builder' ) }</p> }
