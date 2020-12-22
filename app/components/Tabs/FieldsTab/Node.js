ea446ebbbb5af154628cf1430d9f326ba10f54b2
Anh Tran <anhtnt@elightup.com>
2020-12-22 15:05:12 +0700

    Remove reorder handle
    Revert behavior on click to reveal settings, drag field to reorder

--- app/components/Tabs/FieldsTab/Node.js
+++ app/components/Tabs/FieldsTab/Node.js
@@ -12,30 +12,36 @@ const Node = ( { id, field, parent = '', index, removeField, duplicateField } ) 
 	const toggleSettings = () => setExpanded( prev => !prev );
 	const { register } = useFormContext();
 
+	const remove = e => {
+		e.stopPropagation();
+		removeField( id );
+	}
+
+	const duplicate = e => {
+		e.stopPropagation();
+		duplicateField( id );
+	}
+
 	return <Draggable draggableId={ id } index={ index }>
 		{ ( provided, snapshot ) => (
 			<div
 				className={ `og-item og-collapsible${ expanded ? ' og-collapsible--expanded' : '' }${ snapshot.isDragging ? ' og-item--dragging' : '' }` }
 				{ ...provided.draggableProps }
+				{ ...provided.dragHandleProps }
+				ref={ provided.innerRef }
+				title={ __( 'Drag to reorder. Click to reveal field settings.', 'meta-box-builder' ) }
+				onClick={ toggleSettings }
 			>
 				<input ref={ register } type="hidden" name={ `fields${ parent }[${ id }][_id]` } defaultValue={ id } />
 				<input ref={ register } type="hidden" name={ `fields${ parent }[${ id }][type]` } defaultValue={ field.type } />
 				<input ref={ register } type="checkbox" readOnly style={ { display: 'none' } } name={ `fields${ parent }[${ id }][expanded]` } checked={ expanded } />
 				<div className="og-item__header og-collapsible__header">
-					<span
-						className="og-item__handle"
-						title={ __( 'Drag this field to reorder', 'meta-box-builder' ) }
-						ref={ provided.innerRef }
-						{ ...provided.dragHandleProps }
-					>
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M7 10H11V14H7zM7 4H11V8H7zM7 16H11V20H7zM13 10H17V14H13zM13 4H17V8H13zM13 16H17V20H13z" /></svg>
-					</span>
 					<span className="og-item__title" id={ `og-item__title__${ id }` }>{ field.name || __( '(No label)', 'meta-box-builder' ) }</span>
 					<span className="og-item__actions">
 						<span className="og-item__type">{ field.type }</span>
-						<span className="og-item__action og-item__action--remove" title={ __( 'Remove', 'meta-box-builder' ) } onClick={ () => removeField( id ) }><Dashicon icon="trash" /></span>
-						<span className="og-item__action og-item__action--duplicate" title={ __( 'Duplicate', 'meta-box-builder' ) } onClick={ () => duplicateField( id ) }><Dashicon icon="admin-page" /></span>
-						<span className="og-item__action og-item__action--toggle" title={ __( 'Toggle settings', 'meta-box-builder' ) } onClick={ toggleSettings }><Dashicon icon="arrow-down-alt2" /></span>
+						<span className="og-item__action og-item__action--remove" title={ __( 'Remove', 'meta-box-builder' ) } onClick={ remove }><Dashicon icon="trash" /></span>
+						<span className="og-item__action og-item__action--duplicate" title={ __( 'Duplicate', 'meta-box-builder' ) } onClick={ duplicate }><Dashicon icon="admin-page" /></span>
+						<span className="og-item__action og-item__action--toggle" title={ __( 'Toggle settings', 'meta-box-builder' ) }><Dashicon icon="arrow-down-alt2" /></span>
 					</span>
 				</div>
 				{
