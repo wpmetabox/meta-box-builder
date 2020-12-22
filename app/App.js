0105fe6589a7e26556857a73247d8bda14a67b23
Anh Tran <anhtnt@elightup.com>
2020-12-22 10:18:06 +0700

    Fix data not loaded when refreshing pages

--- app/App.js
+++ app/App.js
@@ -1,18 +1,15 @@
 import MainTabs from './components/MainTabs';
 import { Provider as UpdateConditionalProvider } from './context/ConditionalList/ConditionalContext';
 import { FieldsDataProvider } from './context/FieldsDataContext';
-import { Provider as GeneratorProvider } from './context/Generator/GeneratorContext';
 
 const { render } = wp.element;
 
 const App = () => (
-  <FieldsDataProvider>
-    <GeneratorProvider>
-      <UpdateConditionalProvider>
-        <MainTabs />
-      </UpdateConditionalProvider>
-    </GeneratorProvider>
-  </FieldsDataProvider>
+	<FieldsDataProvider>
+		<UpdateConditionalProvider>
+			<MainTabs />
+		</UpdateConditionalProvider>
+	</FieldsDataProvider>
 );
 
 render( <App />, document.getElementById( 'root' ) );
