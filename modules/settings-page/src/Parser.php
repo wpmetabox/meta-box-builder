77cccff62240870608e5e0a1ac96ac027ee27034
Anh Tran <anhtnt@elightup.com>
2021-01-23 15:22:13 +0700

    Fix parsing settings page tabs

--- modules/settings-page/src/Parser.php
+++ modules/settings-page/src/Parser.php
@@ -15,13 +15,8 @@ class Parser extends Base {
 		];
 		$this->settings = array_merge( $this->settings, $settings );
 
-		// Set icon params.
-		$type = Arr::get( $this->settings, 'icon_type', 'dashicons' );
-		$this->icon_url = Arr::get( $this->settings, "icon_$type" );
-		$keys = ['icon_type', 'icon_dashicons', 'icon_svg', 'icon_custom'];
-		foreach ( $keys as $key ) {
-			unset( $this->$key );
-		}
+		$this->parse_menu_icon()
+			->parse_tabs();
 
 		// Cleanup.
 		unset( $this->menu_type );
@@ -32,5 +27,29 @@ class Parser extends Base {
 			->remove_default( 'style', 'boxes' )
 			->remove_default( 'columns', 2 )
 			->remove_default( 'tab_style', 'default' );
+	}
+
+	private function parse_menu_icon() {
+		$type = Arr::get( $this->settings, 'icon_type', 'dashicons' );
+		$this->icon_url = Arr::get( $this->settings, "icon_$type" );
+		$keys = ['icon_type', 'icon_dashicons', 'icon_svg', 'icon_custom'];
+		foreach ( $keys as $key ) {
+			unset( $this->$key );
+		}
+
+		return $this;
+	}
+
+	private function parse_tabs() {
+		if ( empty( $this->tabs ) ) {
+			return;
+		}
+		$tabs = [];
+		foreach ( $this->tabs as $tab ) {
+			$tabs[ $tab['key'] ] = $tab['value'];
+		}
+		$this->tabs = $tabs;
+
+		return $this;
 	}
 }
