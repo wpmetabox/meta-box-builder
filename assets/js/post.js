511e66353e284f27ed0e18bd206c6e242c6c47f1
viet nguyen <viet34tqc@gmail.com>
2021-03-31 11:07:05 +0700

    Fix console.log error. Support id start with number

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,14 +1,18 @@
-( function( document, { meta_box_post_ids, base_url, title } ) {
-	const addIcon = ( [ metaBoxId, postId ] ) => {
+( function ( document, { meta_box_post_ids, base_url, title } ) {
+	const addIcon = ( metaBoxId ) => {
 		let a = document.createElement( 'a' );
-		a.setAttribute( 'href', `${ base_url }${ postId }` );
+		a.setAttribute( 'href', `${ base_url }${ meta_box_post_ids[ metaBoxId ] }` );
 		a.setAttribute( 'class', 'dashicons dashicons-admin-generic mbb-settings' );
 		a.setAttribute( 'title', title );
-		let actions = document.querySelector( `#${ metaBoxId } .handle-actions` );
+		let metabox = document.getElementById( metaBoxId );
+		if ( !metabox ) {
+			return;
+		}
+		let actions = metabox.querySelector( '.handle-actions' );
 		if ( actions ) {
 			actions.prepend( a );
 		}
 	};
 
-	Object.entries( meta_box_post_ids ).forEach( addIcon );
+	Object.keys( meta_box_post_ids ).forEach( addIcon );
 } )( document, MBB );
