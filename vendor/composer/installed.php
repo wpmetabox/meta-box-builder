6b08bdf1a7df31e5cd667e24277c5c0fd0fc31d1
LAPTOP-2R075DES\HP Pavilion <lexuanbao161096@gmail.com>
2021-10-19 10:57:42 +0700

    update danh sách Dashicons

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,53 +1,54 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '74faf173ec666216874dba3bf1f241e2ab710493',
+        'name' => 'meta-box/meta-box-builder',
+        'dev' => true,
     ),
-    'reference' => '497ae15b8ca967a01aae8ff2dd9dd2af2191cb0a',
-    'name' => 'meta-box/meta-box-builder',
-  ),
-  'versions' => 
-  array (
-    'meta-box/mbb-parser' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '5bc051263a062490d051d27ec184b32a91bb688f',
+    'versions' => array(
+        'meta-box/mbb-parser' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../meta-box/mbb-parser',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '5bc051263a062490d051d27ec184b32a91bb688f',
+            'dev_requirement' => false,
+        ),
+        'meta-box/meta-box-builder' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '74faf173ec666216874dba3bf1f241e2ab710493',
+            'dev_requirement' => false,
+        ),
+        'meta-box/support' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../meta-box/support',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '3a02b1a2391afeb31751241d38606b2b0368da7b',
+            'dev_requirement' => false,
+        ),
+        'riimu/kit-phpencoder' => array(
+            'pretty_version' => 'v2.4.1',
+            'version' => '2.4.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../riimu/kit-phpencoder',
+            'aliases' => array(),
+            'reference' => 'ca6f004e1290aec7ef4bebf6c0807b30fcf981d7',
+            'dev_requirement' => false,
+        ),
     ),
-    'meta-box/meta-box-builder' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '497ae15b8ca967a01aae8ff2dd9dd2af2191cb0a',
-    ),
-    'meta-box/support' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '7be71d73b2dd0e8d02b30e4d29a178096a3e0c99',
-    ),
-    'riimu/kit-phpencoder' => 
-    array (
-      'pretty_version' => 'v2.4.1',
-      'version' => '2.4.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'ca6f004e1290aec7ef4bebf6c0807b30fcf981d7',
-    ),
-  ),
 );
