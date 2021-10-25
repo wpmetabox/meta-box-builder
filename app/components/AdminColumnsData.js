89121eaf0afbee58a82a497ef53a4b45242bdfab
Anh Tran <anhtnt@elightup.com>
2021-10-25 09:11:10 +0700

    Format code

--- app/components/AdminColumnsData.js
+++ app/components/AdminColumnsData.js
@@ -3,45 +3,45 @@ const { useContext } = wp.element;
 
 export const AdminColumnsData = () => {
 	const { fieldIds } = useContext( FieldIdsContext );
-	let value          = jQuery(document).find("#settings-object_type").val();
-	jQuery(document).on('change','#settings-object_type', function() {
-		let value  = jQuery(this).val();
-	});
-    if ( value == 'term' ) {
-    	return (
-    		<datalist id="admin-columns">
-    		<option value="cb" />
-    		<option value="name" />
-    		<option value="description" />
-    		<option value="slug" />
-    		<option value="count" />
-    		{ Object.entries( fieldIds ).map( ( [ id, field ] ) => <option key={ id } value={ field.id } /> ) }
-    		</datalist>
-    	);
-    } else if ( value == 'user' ) {
-    	return (
-    		<datalist id="admin-columns">
-    		<option value="cb" />
-    		<option value="username" />
-    		<option value="name" />
-    		<option value="email" />
-    		<option value="role" />
-    		<option value="posts" />
-    		{ Object.entries( fieldIds ).map( ( [ id, field ] ) => <option key={ id } value={ field.id } /> ) }
-    		</datalist>
-    	);
-    } else {
-    	return (
-    		<datalist id="admin-columns">
-    		<option value="cb" />
-    		<option value="title" />
-    		<option value="author" />
-    		<option value="categories" />
-    		<option value="tags" />
-    		<option value="comments" />
-    		<option value="date" />
-    		{ Object.entries( fieldIds ).map( ( [ id, field ] ) => <option key={ id } value={ field.id } /> ) }
-    		</datalist>
-    	);
-    }
+	let value = jQuery( document ).find( '#settings-object_type' ).val();
+	jQuery( document ).on( 'change', '#settings-object_type', function() {
+		value = jQuery( this ).val();
+	} );
+	if ( value == 'term' ) {
+		return (
+			<datalist id="admin-columns">
+				<option value="cb" />
+				<option value="name" />
+				<option value="description" />
+				<option value="slug" />
+				<option value="count" />
+				{ Object.entries( fieldIds ).map( ( [ id, field ] ) => <option key={ id } value={ field.id } /> ) }
+			</datalist>
+		);
+	} else if ( value == 'user' ) {
+		return (
+			<datalist id="admin-columns">
+				<option value="cb" />
+				<option value="username" />
+				<option value="name" />
+				<option value="email" />
+				<option value="role" />
+				<option value="posts" />
+				{ Object.entries( fieldIds ).map( ( [ id, field ] ) => <option key={ id } value={ field.id } /> ) }
+			</datalist>
+		);
+	} else {
+		return (
+			<datalist id="admin-columns">
+				<option value="cb" />
+				<option value="title" />
+				<option value="author" />
+				<option value="categories" />
+				<option value="tags" />
+				<option value="comments" />
+				<option value="date" />
+				{ Object.entries( fieldIds ).map( ( [ id, field ] ) => <option key={ id } value={ field.id } /> ) }
+			</datalist>
+		);
+	}
 };
--- app/controls/AdminColumnsPosition.js
+++ app/controls/AdminColumnsPosition.js
@@ -3,25 +3,25 @@ import DivRow from './DivRow';
 const { __ } = wp.i18n;
 
 const AdminColumnsPosition = ( { name, defaultValue, ...rest } ) => {
-	let value        = jQuery(document).find("#settings-object_type").val();
-	let valueColumn  = 'title';
-	jQuery(document).on('change','#settings-object_type', function() {
-		value = jQuery(this).val();
-	});
-	if ( value == 'term') {
+	let value = jQuery( document ).find( '#settings-object_type' ).val();
+	let valueColumn = 'title';
+	jQuery( document ).on( 'change', '#settings-object_type', function() {
+		value = jQuery( this ).val();
+	} );
+	if ( value == 'term' ) {
 		valueColumn = 'name';
 	} else if ( value == 'user' ) {
 		valueColumn = 'username';
 	}
 	return (
-	    <DivRow { ...rest }>
-		    <select name={ `${ name }[type]` } defaultValue={ dotProp.get( defaultValue, 'type', 'after' ) }>
-			    <option value="after">{ __( 'After', 'meta-box-builder' ) }</option>
-			    <option value="before">{ __( 'Before', 'meta-box-builder' ) }</option>
-			    <option value="replace">{ __( 'Replace', 'meta-box-builder' ) }</option>
-		    </select>
-		    <input type="text" name={ `${ name }[column]` } defaultValue={ dotProp.get( defaultValue, 'column', valueColumn ) } list="admin-columns" />
-	    </DivRow>
+		<DivRow { ...rest }>
+			<select name={ `${ name }[type]` } defaultValue={ dotProp.get( defaultValue, 'type', 'after' ) }>
+				<option value="after">{ __( 'After', 'meta-box-builder' ) }</option>
+				<option value="before">{ __( 'Before', 'meta-box-builder' ) }</option>
+				<option value="replace">{ __( 'Replace', 'meta-box-builder' ) }</option>
+			</select>
+			<input type="text" name={ `${ name }[column]` } defaultValue={ dotProp.get( defaultValue, 'column', valueColumn ) } list="admin-columns" />
+		</DivRow>
 	);
 };
 
