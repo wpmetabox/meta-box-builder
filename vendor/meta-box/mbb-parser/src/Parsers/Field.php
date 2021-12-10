b358faec6ee38e02cda9016ad92a5c8b2b1bac9c
lexuanbao161096 <lexuanbao161096@gmail.com>
2021-12-10 11:18:47 +0700

    check thêm điều kiện

--- vendor/meta-box/mbb-parser/src/Parsers/Field.php
+++ vendor/meta-box/mbb-parser/src/Parsers/Field.php
@@ -108,15 +108,12 @@ class Field extends Base {
 		}
 
 		// Use callback: function_name format.
-        /*
 		if ( is_string( $this->options ) && 0 === strpos( $this->options, 'callback:' ) ) {
 			$callback = trim( str_replace( 'callback:', '', $this->options ) );
 			if ( is_callable( $callback ) ) {
-				$this->options = call_user_func( $callback );
 				$this->_callback = $callback; // For using in the encoders.
 			}
-			return $this;
-		}*/
+		}
 
 		$options = array();
 
