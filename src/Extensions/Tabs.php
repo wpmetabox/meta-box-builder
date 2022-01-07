d922b4f6522d588e9ef3c99ff5c941f272918d33
Anh Tran <anhtnt@elightup.com>
2022-01-07 17:37:18 +0700

    Sửa khi value của icon ko có prefix dashicons- thì tự động thêm
    Không cần phải sửa trong plugin Meta Box Tabs.

--- src/Extensions/Tabs.php
+++ src/Extensions/Tabs.php
@@ -62,14 +62,18 @@ class Tabs {
 			if ( 'tab' !== Arr::get( $field, 'type' ) ) {
 				continue;
 			}
-			$label = Arr::get( $field, 'name', '' );
-			$icon  = Arr::get( $field, 'icon', '' );
 
-			if ( 'url' === Arr::get( $field, 'icon_type' ) ) {
-				$icon = Arr::get( $field, 'icon_url', '' );
-			}
-			if ( 'fontawesome' === Arr::get( $field, 'icon_type' ) ) {
-				$icon = Arr::get( $field, 'icon_fa', '' );
+			$type   = Arr::get( $field, 'icon_type', 'dashicons' );
+			$label  = Arr::get( $field, 'name', '' );
+			$params = [
+				'dashicons'   => 'icon',
+				'url'         => 'icon_url',
+				'fontawesome' => 'icon_fa',
+			];
+			$icon   = Arr::get( $field, $params[ $type ], '' );
+
+			if ( $type === 'dashicons' && $icon ) {
+				$icon = "dashicons-$icon";
 			}
 
 			$tabs[ $field['id'] ] = compact( 'label', 'icon' );
