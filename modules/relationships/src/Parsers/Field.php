a02802defacdaac8ca431dbcb4fa65a16afd0d2d
kiet_DEV <tuankiet2605@gmail.com>
2022-04-09 13:57:20 +0700

    Fix parse FIeld

--- modules/relationships/src/Parsers/Field.php
+++ modules/relationships/src/Parsers/Field.php
@@ -5,6 +5,6 @@ use MBBParser\Parsers\Base;
 
 class Field extends Base {
 	public function parse() {
-		$this->remove_empty_values();
+		$this->parse_array_attributes( 'query_args' )->remove_empty_values();
 	}
 }
