6ac283486e41064dbd252362b08e7422e817246c
Anh Tran <anhtnt@elightup.com>
2022-10-06 15:51:14 +0700

    Format code

--- modules/settings-page/src/Edit.php
+++ modules/settings-page/src/Edit.php
@@ -5,13 +5,12 @@ use MBB\BaseEditPage;
 use MetaBox\Support\Data;
 
 class Edit extends BaseEditPage {
-
-	public function add_meta_boxes( $meta_boxes ){
-		$request = rwmb_request();
-		$settings = get_post_meta( $request->get('post'), 'settings', true );
+	public function add_meta_boxes( $meta_boxes ) {
+		$request  = rwmb_request();
+		$settings = get_post_meta( $request->get( 'post' ), 'settings', true );
 
 		$meta_boxes[] = [
-			'title'      => __( $this->slug_meta_box_title, 'meta-box-builder' ),
+			'title'      => $this->slug_meta_box_title,
 			'post_types' => [ $this->post_type ],
 			'context'    => 'side',
 			'priority'   => 'low',
@@ -19,19 +18,19 @@ class Edit extends BaseEditPage {
 				[
 					'type' => 'text',
 					'id'   => 'post_name',
-					'name' => __( 'ID', 'meta-box-builder' )
-				],				
+					'name' => __( 'ID', 'meta-box-builder' ),
+				],
 				[
 					'type' => 'custom_html',
-					'std'  => '<a class="toggle_option_name" href="javascript:void(0)">'.__( 'Advanced', 'meta-box-builder' ).'</a>',
-				],				
+					'std'  => '<a class="toggle_option_name" href="javascript:void(0)">' . __( 'Advanced', 'meta-box-builder' ) . '</a>',
+				],
 				[
-					'type' => 'text',
-					'id'=> 'settings[option_name]',
-					'name'   => __( 'Option name', 'meta-box-builder' ),
-					'tooltip' => __( 'Option name where settings data is saved to. Takes settings page ID if missed. If you want to use theme mods, then set this to <code>theme_mods_$themeslug</code>.', 'meta-box-builder' ),					
-					'std' => !empty( $settings ) && isset( $settings['option_name'] ) ? $settings['option_name'] : ''
-				]
+					'type'    => 'text',
+					'id'      => 'settings[option_name]',
+					'name'    => __( 'Option name', 'meta-box-builder' ),
+					'tooltip' => __( 'Option name where settings data is saved to. Takes settings page ID if missed. If you want to use theme mods, then set this to <code>theme_mods_$themeslug</code>.', 'meta-box-builder' ),
+					'std'     => ! empty( $settings ) && isset( $settings['option_name'] ) ? $settings['option_name'] : '',
+				],
 			],
 		];
 		return $meta_boxes;
@@ -40,17 +39,17 @@ class Edit extends BaseEditPage {
 	public function enqueue() {
 		$url = MBB_URL . 'modules/settings-page/assets';
 
-		wp_enqueue_style( 'mb-settings-page-ui', "$url/settings-page.css", ['wp-components'], MBB_VER );
+		wp_enqueue_style( 'mb-settings-page-ui', "$url/settings-page.css", [ 'wp-components' ], MBB_VER );
 
-		wp_enqueue_code_editor( ['type' => 'application/x-httpd-php'] );
-		wp_enqueue_script( 'mb-settings-page-ui', "$url/settings-page.js", ['jquery', 'wp-element', 'wp-components', 'wp-i18n', 'clipboard'], MBB_VER, true );
+		wp_enqueue_code_editor( [ 'type' => 'application/x-httpd-php' ] );
+		wp_enqueue_script( 'mb-settings-page-ui', "$url/settings-page.js", [ 'jquery', 'wp-element', 'wp-components', 'wp-i18n', 'clipboard' ], MBB_VER, true );
 
 		$data = [
-			'settings' => get_post_meta( get_the_ID(), 'settings', true ),
-			'icons'    => Data::get_dashicons(),
+			'settings'       => get_post_meta( get_the_ID(), 'settings', true ),
+			'icons'          => Data::get_dashicons(),
 
-			'rest'  => untrailingslashit( rest_url() ),
-			'nonce' => wp_create_nonce( 'wp_rest' ),
+			'rest'           => untrailingslashit( rest_url() ),
+			'nonce'          => wp_create_nonce( 'wp_rest' ),
 
 			'menu_positions' => $this->get_menu_positions(),
 			'menu_parents'   => $this->get_menu_parents(),
