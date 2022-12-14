3578382d7a1b00b32b61e555e6f8b600092e30ee
Anh Tran <anhtnt@elightup.com>
2022-12-14 22:49:41 +0700

    Set default post type if empty

--- app/hooks/usePostTypes.js
+++ app/hooks/usePostTypes.js
@@ -1,11 +1,17 @@
 import create from 'zustand';
 import { ensureArray, getSettings } from '../functions';
 
+const sanitize = types => {
+	// Remove empty value.
+	let t = types.filter( type => /\S/.test( type ) );
+	return t.length > 0 ? t : [ 'post' ];
+};
+
 const settings = getSettings();
 
 const usePostTypes = create( set => ( {
-	types: ensureArray( settings.post_types || [ 'post' ] ),
-	update: types => set( state => ( { types } ) )
+	types: ensureArray( sanitize( settings.post_types ) ),
+	update: types => set( state => ( { types: sanitize( types ) } ) ),
 } ) );
 
 export default usePostTypes;
