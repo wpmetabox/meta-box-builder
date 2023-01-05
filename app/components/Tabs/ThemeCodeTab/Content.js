76d1dea659637bb09e0806249edd905182210db4
Anh Tran <anhtnt@elightup.com>
2023-01-05 15:44:35 +0700

    Update copy code button

--- app/components/Tabs/ThemeCodeTab/Content.js
+++ app/components/Tabs/ThemeCodeTab/Content.js
@@ -1,23 +1,16 @@
-import { ClipboardButton } from "@wordpress/components";
+import { useCopyToClipboard } from "@wordpress/compose";
+import { useState } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
 import { UnControlled as CodeMirror } from "react-codemirror2";
 const { withState } = wp.compose;
 
 const Content = ( { show, codeValue } ) => {
-	const Button = withState( { hasCopied: false } )(
-		( { hasCopied, setState, textCode } ) => (
-			<ClipboardButton
-				className="button"
-				text={ textCode }
-				onCopy={ () => setState( { hasCopied: true } ) }
-				onFinishCopy={ () => setState( { hasCopied: false } ) }
-			>
-				{ hasCopied
-					? __( 'Copied!', 'meta-box-builder' )
-					: __( 'Copy', 'meta-box-builder' ) }
-			</ClipboardButton>
-		)
-	);
+	const [ copied, setCopied ] = useState( false );
+
+	const copyRef = useCopyToClipboard( codeValue, () => {
+		setCopied( true );
+		setTimeout( () => setCopied( false ), 2000 );
+	} );
 
 	return (
 		<div className="og-result__body">
@@ -34,7 +27,9 @@ const Content = ( { show, codeValue } ) => {
 				} }
 			/>
 
-			<Button textCode={ codeValue } />
+			<button type="button" className="button" text={ codeValue } ref={ copyRef }>
+				{ copied ? __( 'Copied!', 'meta-box-builder' ) : __( 'Copy', 'meta-box-builder' ) }
+			</button>
 		</div>
 	);
 };
