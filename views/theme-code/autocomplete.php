68cf5bd71c880b523fa0c3938cd499382ee8a2c0
Anh Tran <anhtnt@elightup.com>
2023-01-27 11:12:34 +0700

    Fix indent for autocomplete

--- views/theme-code/autocomplete.php
+++ views/theme-code/autocomplete.php
@@ -2,71 +2,70 @@
 if ( $is_group === true ) {
 	// Displaying in group
 	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-	
+
 	return;
 }
 
 if ( ! isset( $field['clone'] ) ) {
 	// Displaying selected values:
-	$this->out( '<?php', false );
-	$this->out( '// Displaying selected values:' );
-	$this->out( "\$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-	$this->out( '?>', false );
-	$this->out( '<ul>', false );
-	$this->out( '<?php foreach ( $values as $value ) : ?>', false );
+	$this->out( '<?php', 0 );
+	$this->out( '// Displaying selected values:', 0 );
+	$this->out( "\$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
+	$this->out( '?>', 0 );
+	$this->out( '<ul>', 0 );
+	$this->out( '<?php foreach ( $values as $value ) : ?>', 0 );
 	$this->out( '<li><?= $value ?></li>' );
-	$this->out( '<?php endforeach ?>', false );
-	$this->out( '</ul>', false );
+	$this->out( '<?php endforeach ?>', 0 );
+	$this->out( '</ul>', 0 );
 	$this->break();
 
 	// Displaying selected labels:
-	$this->out( '<?php // Displaying selected labels: ?>', false );
-	$this->out( '<p>Choices:</p>', false );
-	$this->out( "<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ) ?>', false );
+	$this->out( '<?php // Displaying selected labels: ?>', 0 );
+	$this->out( '<p>Choices:</p>', 0 );
+	$this->out( "<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ) ?>', 0 );
 	$this->break();
 
 	// Displaying both values and labels:
-	$this->out( '<?php', false );
-	$this->out( '// Displaying both values and labels:' );
-	$this->out( "\$field   = rwmb_get_field_settings( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-	$this->out( "\$options = \$field['options'];" );
-	$this->out( "\$values  = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+	$this->out( '<?php', 0 );
+	$this->out( '// Displaying both values and labels:', 0 );
+	$this->out( "\$field   = rwmb_get_field_settings( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
+	$this->out( "\$options = \$field['options'];", 0 );
+	$this->out( "\$values  = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
 	$this->out( '?>', false );
 
-	$this->out( '<ul>', false );
-	$this->out( '<?php foreach ( $values as $value ) : ?>' );
-	$this->out( $this->indent() . '<li>' );
-	$this->out( $this->indent( 2 ) . 'Value: <?= $value ?><br>' );
-	$this->out( $this->indent( 2 ) . 'Label: <?= $options[ $value ] ?>' );
-	$this->out( $this->indent() . '</li>' );
-	$this->out( '<?php endforeach ?>' );
-	$this->out( '</ul>', false );
+	$this->out( '<ul>', 0 );
+	$this->out( '<?php foreach ( $values as $value ) : ?>', 1 );
+	$this->out( '<li>', 2 );
+	$this->out( 'Value: <?= $value ?><br>', 3 );
+	$this->out( 'Label: <?= $options[ $value ] ?>', 3 );
+	$this->out( '</li>', 2 );
+	$this->out( '<?php endforeach ?>', 1 );
+	$this->out( '</ul>', 0 );
 
-	$this->out( '?>', false, false );
+	$this->out( '?>', 0 );
 	return;
-
 }
 
 // Displaying cloneable values:
-$this->out( '<?php', false );
-$this->out( "\$field   = rwmb_get_field_settings( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( "\$options = \$field['options'];" );
-$this->out( "\$values  = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', false );
+$this->out( '<?php', 0 );
+$this->out( "\$field   = rwmb_get_field_settings( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
+$this->out( "\$options = \$field['options'];", 0 );
+$this->out( "\$values  = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
+$this->out( '?>', 0 );
 
-$this->out( '<ul>', false );
-$this->out( $this->indent() . '<?php foreach ( $values as $clone ) : ?>', false );
-$this->out( $this->indent() . '<li>' );
-$this->out( $this->indent( 2 ) . '<ul>' );
+$this->out( '<ul>', 0 );
+$this->out( '<?php foreach ( $values as $clone ) : ?>', 1 );
+$this->out( '<li>', 2 );
+$this->out( '<ul>', 3 );
 
-$this->out( $this->indent( 3 ) . '<?php foreach ( $clone as $value ) : ?>' );
-$this->out( $this->indent( 4 ) . '<li>' );
-$this->out( $this->indent( 5 ) . 'Value: <?= $value ?><br>' );
-$this->out( $this->indent( 5 ) . 'Label: <?= $options[ $value ] ?>' );
-$this->out( $this->indent( 4 ) . '</li>' );
-$this->out( $this->indent( 3 ) . '<?php endforeach ?>' );
+$this->out( '<?php foreach ( $clone as $value ) : ?>', 4 );
+$this->out( '<li>', 5 );
+$this->out( 'Value: <?= $value ?><br>', 6 );
+$this->out( 'Label: <?= $options[ $value ] ?>', 6 );
+$this->out( '</li>', 5 );
+$this->out( '<?php endforeach ?>', 4 );
 
-$this->out( $this->indent( 2 ) . '</ul>' );
-$this->out( $this->indent() . '</li>' );
-$this->out( $this->indent() . '<?php endforeach ?>', false );
-$this->out( '</ul>', false, false );
+$this->out( '</ul>', 3 );
+$this->out( '</li>', 2 );
+$this->out( '<?php endforeach ?>', 1 );
+$this->out( '</ul>', 0 );
