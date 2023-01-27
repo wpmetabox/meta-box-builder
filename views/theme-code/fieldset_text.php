69dbe39e92ede38233fc045d4d383290bf5338d5
Anh Tran <anhtnt@elightup.com>
2023-01-27 15:14:08 +0700

    Done fieldset_text

--- views/theme-code/fieldset_text.php
+++ views/theme-code/fieldset_text.php
@@ -2,30 +2,30 @@
 if ( $is_group === true ) {
 	// Displaying in group
 	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-	
+
 	return;
 }
 
-if ( isset( $field['clone'] ) ) {
+if ( ! empty( $field['clone'] ) ) {
 	// Displaying cloneable values:
-	$this->out( '<?php', 0 );
+	$this->out( '<?php' );
 	$this->out( '// Displaying field inputs\' values:' );
 	$this->out( "\$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-	$this->out( '?>', 0 );
+	$this->out( '?>' );
 
-	$this->out( '<?php foreach ( $values as $value ) : ?>', 0 );
-	$this->out( "<p>Name: <?= \$value['name'] ?></p>" );
-	$this->out( "<p>Address: <?= \$value['address'] ?></p>" );
-	$this->out( "<p>Email: <?= \$value['email'] ?></p>" );
+	$this->out( '<?php foreach ( $values as $value ) : ?>' );
+		$this->out( "<p>Name: <?= \$value['name'] ?></p>", 1 );
+		$this->out( "<p>Address: <?= \$value['address'] ?></p>", 1 );
+		$this->out( "<p>Email: <?= \$value['email'] ?></p>", 1 );
 	$this->out( '<?php endforeach ?>', 0, 0 );
 	return;
 }
 
 // Displaying field inputs' values:
-$this->out( '<?php', 0 );
+$this->out( '<?php' );
 $this->out( '// Displaying field inputs\' values:' );
 $this->out( "\$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', 0 );
-$this->out( "<p>Name: <?= \$value['name'] ?></p>", 0 );
-$this->out( "<p>Address: <?= \$value['address'] ?></p>", 0 );
+$this->out( '?>' );
+$this->out( "<p>Name: <?= \$value['name'] ?></p>" );
+$this->out( "<p>Address: <?= \$value['address'] ?></p>" );
 $this->out( "<p>Email: <?= \$value['email'] ?></p>", 0, 0 );
