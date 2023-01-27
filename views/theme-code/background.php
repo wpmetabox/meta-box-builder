9220459dd57974d6c722839910ba3269f71738bf
Anh Tran <anhtnt@elightup.com>
2023-01-27 14:44:20 +0700

    Remove default indent

--- views/theme-code/background.php
+++ views/theme-code/background.php
@@ -8,17 +8,16 @@ if ( $is_group === true ) {
 
 if ( empty( $field['clone'] ) ) {
 	// Getting the background properties:
-	$this->out( '<?php', 0 );
-	$this->out( '// Getting the background properties:', 0 );
-	$this->out( "\$background = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
-	$this->out( "echo \$background['color'];", 0 );
-	$this->out( "echo \$background['image'];", 0 );
-	$this->out( '?>', 0 );
-	$this->break();
+	$this->out( '<?php' );
+	$this->out( '// Getting the background properties:' );
+	$this->out( "\$background = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+	$this->out( "echo \$background['color'];" );
+	$this->out( "echo \$background['image'];" );
+	$this->out( '?>', 0, 3 );
 
 	// Outputting the CSS for the background:
-	$this->out( '<?php // Outputting the CSS for the background: ?>', 0 );
-	$this->out( "<div style=\"<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>">', 0 );
+	$this->out( '<?php // Outputting the CSS for the background: ?>' );
+	$this->out( "<div style=\"<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>">' );
 	$this->out( '<h2>My section title</h2>', 1 );
 	$this->out( '<p>My section content</p>', 1 );
 	$this->out( '</div>', 0, 0 );
--- views/theme-code/checkbox.php
+++ views/theme-code/checkbox.php
@@ -8,33 +8,33 @@ if ( $is_group === true ) {
 
 if ( ! empty( $field['clone'] ) ) {
 	// Displaying cloneable values:
-	$this->out( '<?php', 0 );
-	$this->out( "\$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
-	$this->out( 'foreach ( $values as $value ) {', 0 );
-	$this->out( 'if ( $value ) {', 1 );
-	$this->out( 'echo \'Checked\';', 2 );
-	$this->out( '} else {', 1 );
-	$this->out( 'echo \'Unchecked\';', 2 );
-	$this->out( '}', 1 );
-	$this->out( '}', 0 );
+	$this->out( '<?php' );
+	$this->out( "\$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+	$this->out( 'foreach ( $values as $value ) {' );
+		$this->out( 'if ( $value ) {', 1 );
+			$this->out( 'echo \'Checked\';', 2 );
+		$this->out( '} else {', 1 );
+			$this->out( 'echo \'Unchecked\';', 2 );
+		$this->out( '}', 1 );
+	$this->out( '}' );
 	$this->out( '?>', 0, 0 );
 	return;
 }
 
 // Conditional check:
-$this->out( '<?php', 0 );
-$this->out( '// Conditional check:', 0 );
-$this->out( "\$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
-$this->out( 'if ( $value ) {', 0 );
-$this->out( 'echo \'Checked\';', 1 );
-$this->out( '} else {', 0 );
-$this->out( 'echo \'Unchecked\';', 1 );
-$this->out( '}', 0 );
-$this->out( '?>', 0, 2 );
+$this->out( '<?php' );
+$this->out( '// Conditional check:' );
+$this->out( "\$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+$this->out( 'if ( $value ) {' );
+	$this->out( 'echo \'Checked\';', 1 );
+$this->out( '} else {' );
+	$this->out( 'echo \'Unchecked\';', 1 );
+$this->out( '}' );
+$this->out( '?>', 0, 3 );
 
 // Displaying "Yes/No":
-$this->out( '<?php', 0 );
-$this->out( '// Displaying "Yes/No":', 0 );
-$this->out( "rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
+$this->out( '<?php' );
+$this->out( '// Displaying "Yes/No":' );
+$this->out( "rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
 $this->out( '?>', 0, 0 );
 
