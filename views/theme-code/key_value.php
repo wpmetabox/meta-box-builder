afe878914fa90b5fb87c1194eb6ae42f20233c1e
Anh Tran <anhtnt@elightup.com>
2023-01-27 15:43:55 +0700

    Done key value

--- views/theme-code/key_value.php
+++ views/theme-code/key_value.php
@@ -2,24 +2,23 @@
 if ( $is_group === true ) {
 	// Displaying in group
 	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-	
+
 	return;
 }
 
 // Displaying list of key-value pairs:
-$this->out( '<?php', 0 );
+$this->out( '<?php' );
 $this->out( '// Displaying list of key-value pairs:' );
 $this->out( "\$pairs = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', 0 );
-$this->out( '<h3>Specification</h3>', 0 );
-$this->out( '<ul>', 0 );
-$this->out( '<?php foreach ( $pairs as $pair ) : ?>' );
-$this->out( $this->indent() . '<li><label><?= $pair[0] ?>:</label> <?= $pair[1] ?></li>' );
-$this->out( '<?php endforeach ?>' );
-$this->out( '</ul>', 0 );
-$this->break();
+$this->out( '?>' );
+$this->out( '<h3>Specification</h3>' );
+$this->out( '<ul>' );
+	$this->out( '<?php foreach ( $pairs as $pair ) : ?>', 1 );
+		$this->out( '<li><label><?= $pair[0] ?>:</label> <?= $pair[1] ?></li>', 2 );
+	$this->out( '<?php endforeach ?>', 1 );
+$this->out( '</ul>', 0, 3 );
 
 // or simpler:
-$this->out( '<?php // or simpler: ?>', 0 );
-$this->out( '<h3>Specification</h3>', 0 );
+$this->out( '<?php // or simpler: ?>' );
+$this->out( '<h3>Specification</h3>' );
 $this->out( "<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 0, 0 );
