08453795631aec3371b5a6c9d04fb50fc73dd094
Anh Tran <anhtnt@elightup.com>
2023-01-28 14:10:12 +0700

    Done text list

--- views/theme-code/text_list.php
+++ views/theme-code/text_list.php
@@ -2,33 +2,32 @@
 if ( $is_group === true ) {
 	// Displaying in group
 	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-	
+
 	return;
 }
 
 if ( isset( $field['clone'] ) ) {
 	// Displaying cloneable values:
-	$this->out( "<?php \$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ) ?>', 0 );
-	$this->out( '<ul>', 0 );
-	$this->out( '<?php foreach ( $values as $value ) : ?>' );
-	$this->out( $this->indent() . '<li>' );
-	$this->out( $this->indent( 2 ) . '<span>Name: <?= $value[0] ?></span>' );
-	$this->out( $this->indent( 2 ) . '<span>Email: <?= $value[1] ?></span>' );
-	$this->out( $this->indent() . '</li>' );
-	$this->out( '<?php endforeach ?>' );
+	$this->out( "<?php \$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ) ?>' );
+	$this->out( '<ul>' );
+		$this->out( '<?php foreach ( $values as $value ) : ?>', 1 );
+			$this->out( '<li>', 2 );
+				$this->out( '<span>Name: <?= $value[0] ?></span>', 3 );
+				$this->out( '<span>Email: <?= $value[1] ?></span>', 3 );
+			$this->out( '</li>', 2 );
+		$this->out( '<?php endforeach ?>', 1 );
 	$this->out( '</ul>', 0, 0 );
 
 	return;
 }
 
 // Displaying field inputs' values:
-$this->out( '<?php // Displaying field inputs\' values: ?>', 0 );
-$this->out( "<?php \$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ) ?>', 0 );
-$this->out( '<span>Name: <?= $value[0] ?></span>', 0 );
-$this->out( '<span>Email: <?= $value[1] ?></span>', 0 );
-$this->break();
+$this->out( '<?php // Displaying field inputs\' values: ?>' );
+$this->out( "<?php \$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ) ?>' );
+$this->out( '<p>Name: <?= $value[0] ?></p>' );
+$this->out( '<p>Email: <?= $value[1] ?></p>', 0, 3 );
 
 // Displaying field values in a table:
-$this->out( '<?php // Displaying field values in a table: ?>', 0 );
-$this->out( '<h3>Values</h3>', 0 );
+$this->out( '<?php // Displaying field values in a table: ?>' );
+$this->out( '<h3>Values</h3>' );
 $this->out( "<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ) ?>', 0, 0 );
