409557faa2fe1d58a095e62efb0fb6816608ef5a
Anh Tran <anhtnt@elightup.com>
2023-01-28 11:28:00 +0700

    Done sidebar

--- views/theme-code/sidebar.php
+++ views/theme-code/sidebar.php
@@ -2,36 +2,35 @@
 if ( $is_group === true ) {
 	// Displaying in group
 	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-	
+
 	return;
 }
 
 if ( isset( $field['clone'] ) ) {
 	// Displaying cloneable values:
-	$this->out( '<?php', 0 );
-	$this->out( "\$sidebar_ids = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
-	$this->out( 'foreach ( $sidebar_ids as $sidebar_id ) :' );
-	$this->out( $this->indent() . 'if ( is_active_sidebar( $sidebar_id ) ) {' );
-	$this->out( $this->indent( 2 ) . 'dynamic_sidebar( $sidebar_id );' );
-	$this->out( $this->indent() . '}' );
-	$this->out( 'endforeach;' );
+	$this->out( '<?php' );
+	$this->out( "\$sidebar_ids = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+	$this->out( 'foreach ( $sidebar_ids as $sidebar_id ) {' );
+		$this->out( 'if ( is_active_sidebar( $sidebar_id ) ) {', 1 );
+			$this->out( 'dynamic_sidebar( $sidebar_id );', 2 );
+		$this->out( '}', 1 );
+	$this->out( '}' );
 	$this->out( '?>', 0, 0 );
 
 	return;
 }
 
 // Display widgets for the selected sidebar:
-$this->out( '<?php // Display widgets for the selected sidebar: ?>', 0 );
-$this->out( '<div class="sidebar">', false );
-$this->out( "<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 0 );
-$this->out( '</div>', 0 );
-$this->break();
+$this->out( '<?php // Display widgets for the selected sidebar: ?>' );
+$this->out( '<div class="sidebar">' );
+	$this->out( "<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 1 );
+$this->out( '</div>', 0, 3 );
 
 // Checking if the selected sidebar has widgets and displaying it:
-$this->out( '<?php', 0 );
+$this->out( '<?php' );
 $this->out( '// Checking if the selected sidebar has widgets and displaying it:' );
-$this->out( "\$sidebar_id = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );', 0 );
+$this->out( "\$sidebar_id = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
 $this->out( 'if ( is_active_sidebar( $sidebar_id ) ) {' );
-$this->out( $this->indent() . 'dynamic_sidebar( $sidebar_id );' );
+	$this->out( 'dynamic_sidebar( $sidebar_id );', 1 );
 $this->out( '}' );
 $this->out( '?>', 0, 0 );
