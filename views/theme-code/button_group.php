476e013037df7ab697454dd5ba15f4556e514e29
Anh Tran <anhtnt@elightup.com>
2023-01-28 11:25:04 +0700

    Done select

--- views/theme-code/button_group.php
+++ views/theme-code/button_group.php
@@ -1,16 +1,2 @@
 <?php
-if ( $is_group === true ) {
-	// Displaying in group
-	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-
-	return;
-}
-
-if ( empty( $field['clone'] ) ) {
-	$file = empty( $field['multiple'] ) ? 'single' : 'multiple';
-} else {
-	$file = empty( $field['multiple'] ) ? 'single-clone' : 'multiple-clone';
-}
-
-require __DIR__ . "/partials/choice/$file.php";
-
+require __DIR__ . '/select.php';
