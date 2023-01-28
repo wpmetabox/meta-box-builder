415cc8e04008e38555babb0697c383c7c8524cdf
Anh Tran <anhtnt@elightup.com>
2023-01-28 11:47:37 +0700

    Done tax and tax advanced

--- views/theme-code/taxonomy.php
+++ views/theme-code/taxonomy.php
@@ -5,71 +5,59 @@ if ( $is_group === true ) {
 	$this->out( '// Get Taxonomy in group' );
 	$this->out( "\$terms = \$group_value[ '" . $field['id'] . "' ] ?? '';" );
 	$this->out( 'foreach ( $terms as $term ) :' );
-	$this->out( $this->indent() . '<p><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></p>' );
+	$this->out( '<p><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></p>' );
 	$this->out( 'endforeach;' );
 
 	return;
 }
 
-if ( isset( $field['clone'] ) ) {
-	// Displaying cloneable values:
-	$this->out( "<?php \$terms = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 0 );
-	$this->out( '<h3>Project categories</h3>', 0 );
-	$this->out( '<ul>', 0 );
-	$this->out( '<?php foreach ( $terms as $term ) : ?>' );
-	$this->out( $this->indent() . '<li><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></li>' );
-	$this->out( '<?php endforeach ?>' );
-	$this->out( '</ul>', 0, 0 );
-
-	return;
-}
-
-if ( isset( $field['multiple'] ) ) {
+if ( ! empty( $field['multiple'] ) ) {
 	// Displaying multiple selected terms:
-	$this->out( "<?php \$terms = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 0 );
-	$this->out( '<h3>Project categories</h3>', 0 );
-	$this->out( '<ul>', 0 );
-	$this->out( '<?php foreach ( $terms as $term ) : ?>' );
-	$this->out( $this->indent() . '<li><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></li>' );
-	$this->out( '<?php endforeach ?>' );
-	$this->out( '</ul>', 0 );
-	$this->break();
+	$this->out( "<?php \$terms = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>' );
+	$this->out( '<h3>Project categories</h3>' );
+	$this->out( '<ul>' );
+		$this->out( '<?php foreach ( $terms as $term ) : ?>', 1 );
+			$this->out( '<li><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></li>', 2 );
+		$this->out( '<?php endforeach ?>', 1 );
+	$this->out( '</ul>', 0, 3 );
 
 	// or simpler:
-	$this->out( '<?php // or simpler: ?>', 0 );
-	$this->out( '<h3>Project categories</h3>', 0 );
-	$this->out( "<h3><?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?></h3>', 0, 0 );
-
+	$this->out( '<?php // or simpler: ?>' );
+	$this->out( '<h3>Project categories</h3>' );
+	$this->out( "<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 0, 0 );
 	return;
 }
 
 // Getting selected term object:
-$this->out( '<?php', 0 );
+$this->out( '<?php' );
 $this->out( '// Getting selected term object:' );
 $this->out( "\$term = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', 0 );
-$this->out( '<pre>', 0 );
-$this->out( '<!-- Show all data from the selected term -->' );
-$this->out( '<?php print_r( $term ); ?>' );
-$this->out( '</pre>', 0 );
-$this->break();
+$this->out( '?>' );
+$this->out( '<pre>' );
+	$this->out( '<!-- Show all data from the selected term -->', 1 );
+	$this->out( '<?php print_r( $term ); ?>', 1 );
+$this->out( '</pre>', 0, 3 );
 
 // Displaying selected term name:
-$this->out( '<?php', 0 );
+$this->out( '<?php' );
 $this->out( '// Displaying selected term name:' );
 $this->out( "\$term = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', 0 );
-$this->out( '<p><?= $term->name; ?></p>', 0 );
-$this->break();
+$this->out( '?>' );
+$this->out( '<p><?= $term->name; ?></p>', 0, 3 );
 
-// Displaying the selected term with link:
-$this->out( '<?php', 0 );
-$this->out( '// Displaying the selected term with link:' );
-$this->out( "\$term = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', 0 );
-$this->out( '<p><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></p>', 0 );
-$this->break();
 
 // or simpler:
-$this->out( '<?php // or simpler: ?>', 0 );
+$this->out( '<?php // or simpler: ?>' );
+$this->out( "<p><?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'], [ 'link' => false ] ) . ' ); ?></p>', 0, 3 );
+
+
+// Displaying the selected term with link:
+$this->out( '<?php' );
+$this->out( '// Displaying the selected term with link:' );
+$this->out( "\$term = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+$this->out( '?>' );
+$this->out( '<p><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></p>', 0, 3 );
+
+// or simpler:
+$this->out( '<?php // or simpler: ?>' );
 $this->out( "<p><?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?></p>', 0, 0 );
--- views/theme-code/taxonomy_advanced.php
+++ views/theme-code/taxonomy_advanced.php
@@ -11,65 +11,44 @@ if ( $is_group === true ) {
 	return;
 }
 
-if ( isset( $field['clone'] ) ) {
+
+if ( ! empty( $field['clone'] ) ) {
+	if ( ! empty( $field['multiple'] ) ) {
+		// Displaying cloneable values:
+		$this->out( "<?php \$clones = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>' );
+
+		$this->out( '<ul>' );
+			$this->out( '<?php foreach ( $clones as $clone ) : ?>', 1 );
+				$this->out( '<li>', 2 );
+					$this->out( '<ul>', 3 );
+						$this->out( '<?php foreach ( $clone as $term ) : ?>', 4 );
+							$this->out( '<li><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></li>', 5 );
+						$this->out( '<?php endforeach ?>', 4 );
+					$this->out( '</ul>', 3 );
+				$this->out( '</li>', 2 );
+			$this->out( '<?php endforeach ?>', 1 );
+		$this->out( '</ul>', 0, 3 );
+
+		// or simpler:
+		$this->out( '<?php // or simpler: ?>' );
+		$this->out( "<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 0, 0 );
+		return;
+	}
+
 	// Displaying cloneable values:
-	$this->out( "<?php \$terms = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 0 );
-	$this->out( '<h3>Project categories</h3>', 0 );
-	$this->out( '<ul>', 0 );
-	$this->out( '<?php foreach ( $terms as $term ) : ?>' );
-	$this->out( $this->indent() . '<li><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></li>' );
-	$this->out( '<?php endforeach ?>' );
-	$this->out( '</ul>', 0, 0 );
-
-	return;
-}
-
-if ( isset( $field['multiple'] ) ) {
-	// Displaying multiple selected terms:
-	$this->out( "<?php \$terms = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 0 );
-	$this->out( '<h3>Project categories</h3>', 0 );
-	$this->out( '<ul>', 0 );
-	$this->out( '<?php foreach ( $terms as $term ) : ?>' );
-	$this->out( $this->indent() . '<li><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></li>' );
-	$this->out( '<?php endforeach ?>' );
-	$this->out( '</ul>', 0 );
-	$this->break();
+	$this->out( "<?php \$terms = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>' );
+	$this->out( '<h3>Project categories</h3>' );
+	$this->out( '<ul>' );
+		$this->out( '<?php foreach ( $terms as $term ) : ?>', 1 );
+			$this->out( '<li><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></li>', 2 );
+		$this->out( '<?php endforeach ?>', 1 );
+	$this->out( '</ul>', 0, 3 );
 
 	// or simpler:
-	$this->out( '<?php // or simpler: ?>', 0 );
-	$this->out( '<h3>Project categories</h3>', 0 );
-	$this->out( "<h3><?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?></h3>', 0, 0 );
-
+	$this->out( '<?php // or simpler: ?>' );
+	$this->out( '<h3>Project categories</h3>' );
+	$this->out( "<?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>', 0, 0 );
 	return;
 }
 
-// Getting selected term object:
-$this->out( '<?php', 0 );
-$this->out( '// Getting selected term object:' );
-$this->out( "\$term = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', 0 );
-$this->out( '<pre>', 0 );
-$this->out( '<!-- Show all data from the selected term -->' );
-$this->out( '<?php print_r( $term ); ?>' );
-$this->out( '</pre>', 0 );
-$this->break();
-
-// Displaying selected term name:
-$this->out( '<?php', 0 );
-$this->out( '// Displaying selected term name:' );
-$this->out( "\$term = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', 0 );
-$this->out( '<p><?= $term->name; ?></p>', 0 );
-$this->break();
-
-// Displaying the selected term with link:
-$this->out( '<?php', 0 );
-$this->out( '// Displaying the selected term with link:' );
-$this->out( "\$term = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', 0 );
-$this->out( '<p><a href="<?= get_term_link( $term ) ?>"><?= $term->name ?></a></p>', 0 );
-$this->break();
-
-// or simpler:
-$this->out( '<?php // or simpler: ?>', 0 );
-$this->out( "<p><?php rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?></p>', 0, 0 );
+require __DIR__ . '/taxonomy.php';
