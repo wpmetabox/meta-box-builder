fd219e4cc7b6dccbdffce931590b1d3297046125
Anh Tran <anhtnt@elightup.com>
2023-01-28 14:17:25 +0700

    Done textarea

--- views/theme-code/textarea.php
+++ views/theme-code/textarea.php
@@ -2,37 +2,50 @@
 if ( $is_group === true ) {
 	// Displaying in group
 	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-	
+
 	return;
 }
 
 if ( isset( $field['clone'] ) ) {
 	// Displaying cloneable values:
-	$this->out( "<?php \$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ) ?>', 0 );
-	$this->out( '<?php foreach ( $values as $value ) : ?>', 0 );
-	$this->out( '<p><?= $value ?></p>' );
+	$this->out( "<?php \$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ) ?>' );
+	$this->out( '<?php foreach ( $values as $value ) : ?>' );
+		$this->out( '<p><?= $value ?></p>', 1 );
 	$this->out( '<?php endforeach ?>', 0, 0 );
 
 	return;
 }
 
-// Displaying the value:
-$this->out( '<?php', 0 );
-$this->out( '// Displaying the value:' );
-$this->out( "rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>', 0 );
-$this->break();
-
-// Auto adding paragraphs to the text:
-$this->out( '<?php', 0 );
-$this->out( '// Auto adding paragraphs to the text:' );
-$this->out( "\$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( 'echo wpautop( $value );' );
-$this->out( '?>', 0, 0 );
-
 // Getting the value:
-$this->out( '<?php', 0 );
+$this->out( '<?php' );
 $this->out( '// Getting the value:' );
 $this->out( "\$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
 $this->out( 'echo $value;' );
+$this->out( '?>', 0, 3 );
+
+// Displaying the value:
+$this->out( '<?php' );
+$this->out( '// Displaying the value:' );
+$this->out( "rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+$this->out( '?>', 0, 3 );
+
+// Auto adding paragraphs to the text:
+$this->out( '<?php' );
+$this->out( '// Auto adding paragraphs to the text:' );
+$this->out( "\$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+$this->out( 'echo wpautop( $value );' );
+$this->out( '?>', 0, 3 );
+
+// Parse shortcodes:
+$this->out( '<?php' );
+$this->out( '// Parse shortcodes:' );
+$this->out( "\$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+$this->out( 'echo do_shortcode( $value );' );
+$this->out( '?>', 0, 3 );
+
+// Parse blocks
+$this->out( '<?php' );
+$this->out( '// Parse blocks:' );
+$this->out( "\$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
+$this->out( 'echo do_blocks( $value );' );
 $this->out( '?>', 0, 0 );
