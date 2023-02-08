9ee57cd8d20e3e9fd1077ea99c46f2e2f453392f
tuankiet2605 <tuankiet2605@gmail.com>
2023-02-08 15:28:55 +0700

    checkbox list in group

--- views/theme-code/checkbox_list.php
+++ views/theme-code/checkbox_list.php
@@ -1,10 +1,6 @@
 <?php
-if ( $is_group === true ) {
-	// Displaying in group
-	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-
-	return;
-}
 
 $file = empty( $field['clone'] ) ? 'multiple' : 'multiple-clone';
+$file = $is_group === true ? $file.'-group' : $file;
+
 require __DIR__ . "/partials/choice/$file.php";
