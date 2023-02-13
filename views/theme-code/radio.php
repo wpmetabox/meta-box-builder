33c6fe855cf20c7b27b16862ab2dcad1b3f6e046
tuankiet2605 <tuankiet2605@gmail.com>
2023-02-13 08:30:48 +0700

    Radio in group

--- views/theme-code/radio.php
+++ views/theme-code/radio.php
@@ -1,10 +1,4 @@
 <?php
-if ( $is_group === true ) {
-	// Displaying in group
-	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-
-	return;
-}
-
 $file = empty( $field['clone'] ) ? 'single' : 'single-clone';
+$file = $is_group === true ? $file . '-group' : $file;
 require __DIR__ . "/partials/choice/$file.php";
