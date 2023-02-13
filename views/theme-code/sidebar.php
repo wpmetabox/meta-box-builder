7b161de698e20e08b2306f46ca50b1a1d2ef5b41
tuankiet2605 <tuankiet2605@gmail.com>
2023-02-13 08:42:44 +0700

    Sidebar in group

--- views/theme-code/sidebar.php
+++ views/theme-code/sidebar.php
@@ -2,7 +2,23 @@
 if ( $is_group === true ) {
 	// Displaying in group
 	$this->out( "echo \$group_value[ '" . $field['id'] . "' ] ?? '';" );
+	if ( isset( $field['clone'] ) ) {
+		// Displaying cloneable values:
+		$this->out( "\$sidebar_ids = \$group_value[ '" . $field['id'] . "' ] ?? '';" );
+		$this->out( 'foreach ( $sidebar_ids as $sidebar_id ) {' );
+			$this->out( 'if ( is_active_sidebar( $sidebar_id ) ) {', 1 );
+				$this->out( 'dynamic_sidebar( $sidebar_id );', 2 );
+			$this->out( '}', 1 );
+		$this->out( '}' );
 
+		return;
+	}
+
+	$this->out( '// Checking if the selected sidebar has widgets and displaying it:' );
+	$this->out( "\$sidebar_id = \$group_value[ '" . $field['id'] . "' ] ?? '';" );
+	$this->out( 'if ( is_active_sidebar( $sidebar_id ) ) {' );
+		$this->out( 'dynamic_sidebar( $sidebar_id );', 1 );
+	$this->out( '}' );
 	return;
 }
 
