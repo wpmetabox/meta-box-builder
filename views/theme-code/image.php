f33e0c5f491890227cebae1f05085d2980f99dfb
tuankiet2605 <tuankiet2605@gmail.com>
2023-02-13 07:26:44 +0700

    Images in group

--- views/theme-code/image.php
+++ views/theme-code/image.php
@@ -1,16 +1,4 @@
 <?php
-if ( $is_group === true ) {
-	// Displaying in group
-	$this->break();
-	$this->out('// Get Image in group');
-	$this->out( "\$image_ids = \$group_value[ '" . $field['id'] . "' ] ?? '';" );
-	$this->out( 'foreach ( $image_ids as $image_id ) :' );
-	$this->out( "\$image = RWMB_Image_Field::file_info( \$image_id, ['size' => 'thumbnail'] );" );
-	$this->out( "echo '<img src=\"' . \$image['url'] . '\">';" );
-	$this->out( 'endforeach;' );
-
-	return;
-}
-
 $file = empty( $field['clone'] ) ? 'multiple' : 'multiple-clone';
+$file = $is_group === true ? $file.'-group' : $file;
 require __DIR__ . "/partials/image/$file.php";
---
+++ views/theme-code/partials/image/multiple-group.php
@@ -0,0 +1,12 @@
+<?php
+// Displaying uploaded images:
+$this->out( '// Displaying uploaded images:' );
+$this->out( "\$images = \$group_value[ '" . $field['id'] . "' ] ?? '';" );
+$this->out( '?>' );
+$this->out( '<h3>Uploaded images</h3>' );
+$this->out( '<ul>' );
+	$this->out( '<?php foreach ( $images as $image ) : ?>', 1 );
+		$this->out( '<li><img src="<?= $image[\'url\']; ?>"></li>', 2 );
+	$this->out( '<?php endforeach ?>', 1 );
+$this->out( '</ul>', 0, 2 );
+$this->out( '<?php' );
