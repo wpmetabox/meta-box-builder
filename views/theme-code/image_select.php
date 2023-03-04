5a2c77a36a4640c8f2a3986044dbc2d6c2a5e17d
Anh Tran <anhtnt@elightup.com>
2023-03-04 14:33:21 +0700

    Done image select

--- views/theme-code/image_select.php
+++ views/theme-code/image_select.php
@@ -1,42 +1,6 @@
 <?php
-if ( $is_group === true ) {
-	// Displaying in group
-	$this->out( '' );
-	$this->out('// Get Image in group');
-	$this->out( "\$image_ids = \$group[ '" . $field['id'] . "' ] ?? '';" );
-	$this->out( 'foreach ( $image_ids as $image_id ) :' );
-	$this->out( "\$image = RWMB_Image_Field::file_info( \$image_id, ['size' => 'thumbnail'] );" );
-	$this->out( "echo '<img src=\"' . \$image['url'] . '\">';" );
-	$this->out( 'endforeach;' );
+$file  = empty( $field['multiple'] ) ? 'single' : 'multiple';
+$file .= empty( $field['clone'] ) ? '' : '-clone';
+$file .= $is_group === true ? '-group' : '';
 
-	return;
-}
-
-if ( ! empty( $field['clone'] ) ) {
-	// Displaying cloneable values:
-	$this->out( "<?php \$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>' );
-	$this->out( '<ul>' );
-		$this->out( '<?php foreach ( $clone as $value ) : ?>', 1 );
-			$this->out( '<li><?= $value ?></li>', 2 );
-		$this->out( '<?php endforeach ?>', 1 );
-	$this->out( '</ul>', 0, 0 );
-	return;
-}
-
-// Displaying selected value.
-$this->out( '<?php' );
-$this->out( '// Displaying selected value:' );
-$this->out( "\$value = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( 'echo $value;' );
-$this->out( '?>', 0, 3 );
-
-// Displaying the list of multiple choices:
-$this->out( '<?php' );
-$this->out( '// Displaying the list of multiple choices:' );
-$this->out( "\$values = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-$this->out( '?>' );
-$this->out( '<ul>' );
-	$this->out( '<?php foreach ( $values as $value ) : ?>', 1 );
-		$this->out( '<li><?= $value ?></li>', 2 );
-	$this->out( '<?php endforeach ?>', 1 );
-$this->out( '</ul>', 0, 0 );
+require __DIR__ . "/partials/image-select/$file.php";
---
+++ views/theme-code/partials/image-select/multiple-clone.php
@@ -0,0 +1,15 @@
+<?php
+$this->out( "<?php \$clones = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' ); ?>' );
+$this->out( '<ul>' );
+	$this->out( '<?php foreach ( $clones as $clone ) : ?>', 1 );
+		$this->out( '<li>', 2 );
+
+			$this->out( '<ul>', 3 );
+				$this->out( '<?php foreach ( $clone as $value ) : ?>', 4 );
+					$this->out( '<li><?= $value ?></li>', 5 );
+				$this->out( '<?php endforeach ?>', 4 );
+			$this->out( '</ul>', 3 );
+
+		$this->out( '</li>', 2 );
+	$this->out( '<?php endforeach ?>', 1 );
+$this->out( '</ul>', 0, 0 );
