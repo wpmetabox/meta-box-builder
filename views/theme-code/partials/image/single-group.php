23edb21a50efb25ee9aab54dabbd483f71857916
Anh Tran <anhtnt@elightup.com>
2023-03-06 15:38:49 +0700

    Fix missing ) for single_image

--- views/theme-code/partials/image/single-group.php
+++ views/theme-code/partials/image/single-group.php
@@ -1,6 +1,6 @@
 <?php
 $this->out( "\$image_id = \$group[ '" . $field['id'] . "' ] ?? 0;" );
-$this->out( '$image = RWMB_Image_Field::file_info( $image_id, [ \'size\' => \'thumbnail\' ];' );
+$this->out( '$image = RWMB_Image_Field::file_info( $image_id, [ \'size\' => \'thumbnail\' ] );' );
 $this->out( '?>' );
 $this->out( '<h3>Logo</h3>' );
 $this->out( '<img src="<?= $image[\'url\']; ?>">' );
