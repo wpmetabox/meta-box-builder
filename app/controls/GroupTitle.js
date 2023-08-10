370b487bc89fac04a7879e182771530e794b1d2f
Anh Tran <anhtnt@elightup.com>
2023-08-10 15:45:34 +0700

    Make group title use the nameId hook

--- app/controls/GroupTitle.js
+++ app/controls/GroupTitle.js
@@ -1,25 +1,15 @@
-import { __ } from "@wordpress/i18n";
 import DivRow from './DivRow';
 
-const GroupTitle = ( { name, componentId, defaultValue, ...rest } ) => {
-	const onChange = e => {
-		// Don't update field header bar if it has name.
-		const nameElement = document.getElementById( `fields-${ rest.fieldId }-name` );
-		if ( nameElement.value ) {
-			return;
-		}
+const GroupTitle = ( { name, componentId, nameIdData, ...rest } ) => (
+	<DivRow htmlFor={ componentId } { ...rest }>
+		<input
+			type="text"
+			id={ componentId }
+			name={ name }
+			value={ nameIdData.group_title }
+			onChange={ e => nameIdData.updateGroupTitle( e.target.value ) }
+		/>
+	</DivRow>
+);
 
-		// Update field header bar.
-		const titleElement = e.target.closest( '.og-item' ).querySelector( '.og-item__title' );
-		if ( titleElement ) {
-			titleElement.textContent = e.target.value || __( '(No label)', 'meta-box-builder' );
-		}
-	};
-
-	return (
-		<DivRow htmlFor={ componentId } { ...rest }>
-			<input type="text" id={ componentId } defaultValue={ defaultValue } name={ name } onChange={ onChange } />
-		</DivRow>
-	);
-};
 export default GroupTitle;
