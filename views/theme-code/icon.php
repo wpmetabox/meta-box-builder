c1ebb5c0606d5d50270308f8d5bbec460cb8e20f
Anh Tran <anhtnt@elightup.com>
2023-10-19 14:28:19 +0700

    Format code

--- views/theme-code/icon.php
+++ views/theme-code/icon.php
@@ -1,39 +1,42 @@
 <?php
-// With group
+// With group.
 if ( $in_group ) {
-	// Group with clone
+	// Group with clone.
 	if ( ! empty( $field['clone'] ) ) {
 		$this->out( "\$values = \$group[ '" . $field['id'] . "' ] ?? [];" );
-		$this->out( '// Getting the icon class:' );
-		$this->out( 'foreach ( $values as $value ){' );
-		$this->out( '   echo $value;' );
+		$this->out( '// Getting the icons\' classes:' );
+		$this->out( 'foreach ( $values as $value ) {' );
+			$this->out( 'echo $value;', 1 );
 		$this->out( '}' );
 		return;
 	} else {
 		// Group without clone
+		$this->out( '// Getting the icon class:' );
 		$this->out( "echo \$group[ '" . $field['id'] . "' ] ?? '';" );
 		return;
 	}
 }
-// With clone
+// With clone.
 if ( ! empty( $field['clone'] ) ) {
 	// Getting the icon class:
 	$this->out( '<?php' );
-	$this->out( '// Getting the icon class:' );
+	$this->out( '// Getting the icons\' classes:' );
 	$this->out( "\$values = rwmb_get_value( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
-	$this->out( 'foreach ( $values as $value ){' );
-	$this->out( '   echo $value;' );
+	$this->out( 'foreach ( $values as $value ) {' );
+		$this->out( 'echo $value;', 1 );
 	$this->out( '}' );
 	$this->out( '?>' );
 	return;
 }
-// Single
-// Displaying the icon:
+
+// Single.
+// Displaying the icon.
 $this->out( '<?php' );
 $this->out( '// Displaying the icon:' );
 $this->out( "rwmb_the_value( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
 $this->out( '?>', 0, 2 );
-// Getting the icon class:
+
+// Getting the icon class.
 $this->out( '<?php' );
 $this->out( '// Getting the icon class:' );
 $this->out( "\$value = rwmb_get_value( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
