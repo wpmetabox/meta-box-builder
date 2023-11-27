e3bb1bf207870a760d21bcf95f0ddfac3f42903f
tuankiet2605 <tuankiet2605@gmail.com>
2023-11-27 11:30:04 +0700

    Theme code: Update syntax for group (#68)

--- views/theme-code/group.php
+++ views/theme-code/group.php
@@ -2,7 +2,9 @@
 $subfields = $field['fields'] ?? [];
 if ( isset( $field['clone'] ) ) {
 	// Displaying cloneable group:
-	$this->out( '<?php' );
+	if ( ! $in_group ) {
+		$this->out( '<?php' );
+	}
 	$this->out( "\$groups = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
 	$this->out( 'foreach ( $groups as $group ) {' );
 	++$this->size_indent;
@@ -14,16 +16,22 @@ if ( isset( $field['clone'] ) ) {
 	--$this->size_indent;
 	$this->out( '' );
 	$this->out( '}' );
-	$this->out( '?>', 0, 0 );
+	if ( ! $in_group ) {
+		$this->out( '?>', 0, 0 );
+	}
 	return;
 }
 
 // Displaying the value:
-$this->out( '<?php' );
+if ( ! $in_group ) {
+	$this->out( '<?php' );
+}
 $this->out( "\$group = rwmb_meta( '" . $this->get_encoded_value( $field['id'] ) . ' );' );
 foreach ( $subfields as $sub_field ) {
 	$this->out( '' );
 	$this->out( "// Field {$sub_field['id']}:" );
 	echo $this->get_theme_code( $sub_field, true );
 }
-$this->out( '?>', 0, 0 );
+if ( ! $in_group ) {
+	$this->out( '?>', 0, 0 );
+}
