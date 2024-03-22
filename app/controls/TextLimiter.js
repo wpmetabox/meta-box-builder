9a2a6964548f533dbe345febe08426bd3308dfa1
LyNguyen <cnttnt@gmail.com>
2024-03-22 09:59:30 +0700

    Update code

--- app/controls/TextLimiter.js
+++ app/controls/TextLimiter.js
@@ -3,24 +3,22 @@ import DivRow from './DivRow';
 
 const TextLimiter = ( { defaultValue, componentId, nameIdData, name, ...rest } ) => {
 	return [ 'text', 'textarea', 'wysiwyg' ].includes( nameIdData.type ) && 
-		<>
-			<DivRow { ...rest }>
-				<div className="og-attribute">
-					<input
-						type="text"
-						id={ componentId }
-						name={ `${ name }[limit]` }
-						defaultValue={ defaultValue.limit }
-					/>
-					<div className="og-input">
-						<select name={ `${ name }[limit_type]` } defaultValue={ defaultValue.limit_type || '' }>
-							<option value="character">{ __( 'Characters', 'meta-box-builder' ) }</option>
-							<option value="word">{ __( 'Words', 'meta-box-builder' ) }</option>
-						</select>
-					</div>
+		<DivRow { ...rest }>
+			<div className="og-attribute">
+				<input
+					type="text"
+					id={ componentId }
+					name={ `${ name }[limit]` }
+					defaultValue={ defaultValue.limit }
+				/>
+				<div className="og-input">
+					<select name={ `${ name }[limit_type]` } defaultValue={ defaultValue.limit_type || '' }>
+						<option value="character">{ __( 'Characters', 'meta-box-builder' ) }</option>
+						<option value="word">{ __( 'Words', 'meta-box-builder' ) }</option>
+					</select>
 				</div>
-			</DivRow>
-		</>;
+			</div>
+		</DivRow>;
 };
 
 export default TextLimiter;
