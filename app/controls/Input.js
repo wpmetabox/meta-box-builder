7cef3e9359e1c464587c7ee292a62d3193c56888
LyNguyen <cnttnt@gmail.com>
2024-03-26 18:03:37 +0700

    Add save_format suggestion

--- app/controls/Input.js
+++ app/controls/Input.js
@@ -1,13 +1,21 @@
 import DivRow from './DivRow';
+import FieldInserter from './FieldInserter';
 
-const Input = ( { name, componentId, placeholder, defaultValue, type = 'text', updateFieldData, ...rest } ) => {
+const Input = ( { name, componentId, placeholder, defaultValue, fieldType, type = 'text', updateFieldData, ...rest } ) => {
 	const handleChange = e => updateFieldData && updateFieldData( name, e.target.value );
 
 	return (
 		<DivRow htmlFor={ componentId } { ...rest }>
-			<input type={ type } id={ componentId } name={ name } defaultValue={ defaultValue } onChange={ handleChange } placeholder={ placeholder } required={ rest.required } />
+			{ [ 'datetime', 'time' ].includes( fieldType )
+				? <DateTime  name={ name } defaultValue={ defaultValue } placeholder={ placeholder } required={ rest.required } items={ fieldType === 'time' ? rest.time : rest.datetime } />
+				: <input type={ type } id={ componentId } name={ name } defaultValue={ defaultValue } onChange={ handleChange } placeholder={ placeholder } required={ rest.required } />
+			}
 		</DivRow>
 	);
 };
+
+const DateTime = ( { name, defaultValue, placeholder, required, items } ) => (
+	<FieldInserter required={ required } placeholder={ placeholder } name={ name } defaultValue={ defaultValue } items={ items } />
+);
 
 export default Input;
