94b7e895457ae500dc9c43ca824d90cc058d595d
Anh Tran <anhtnt@elightup.com>
2024-03-29 18:23:47 +0700

    Use description instead of tooltip. Simplify JS component.

--- app/controls/DateTime.js
+++ app/controls/DateTime.js
@@ -1,16 +1,10 @@
-import { useRef, useState } from "@wordpress/element";
 import DivRow from './DivRow';
 import FieldInserter from './FieldInserter';
 
-
-const DateTime = ( { name, componentId, placeholder, defaultValue, fieldType, updateFieldData, ...rest } ) => {
-	return (
-		<DivRow htmlFor={ componentId } { ...rest }>
-			<div className="og-attribute">
-				<FieldInserter name={ name } defaultValue={ defaultValue } required={ rest.required } placeholder={ placeholder } items={ Object.entries( rest[fieldType] ) } />
-			</div>
-		</DivRow>
-	);
-};
+const DateTime = ( { name, componentId, placeholder, defaultValue, fieldType, required, ...rest } ) => (
+	<DivRow htmlFor={ componentId } { ...rest }>
+		<FieldInserter name={ name } defaultValue={ defaultValue } required={ required } placeholder={ placeholder } items={ Object.entries( rest[ fieldType ] ) } />
+	</DivRow>
+);
 
 export default DateTime;
