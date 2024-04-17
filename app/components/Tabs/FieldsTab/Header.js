d08c2ddb8e48d8adbf40722ec0d5a66f5051b568
Tan Nguyen <hi@tan.ng>
2024-04-17 17:26:39 +0700

    fix: use css instead of removing class

--- app/components/Tabs/FieldsTab/Header.js
+++ app/components/Tabs/FieldsTab/Header.js
@@ -2,7 +2,7 @@ import { __ } from "@wordpress/i18n";
 
 const Header = ( { expandAll, toggleAll } ) => (
 	<div className="og-header">
-		<span>&nbsp;</span>
+		<span className="og-column--drag">&nbsp;</span>
 		<span className="og-column--label">
 			{ __( 'Label', 'meta-box-builder' ) }
 			<span
