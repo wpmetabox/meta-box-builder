df21ad06b08d0d423f15c4ce8aa5f1bc5a255e84
LAPTOP-2R075DES\HP Pavilion <lexuanbao161096@gmail.com>
2024-07-09 16:26:33 +0700

    Add an option to allow users to select delete data in DB

--- modules/relationships/app/App.js
+++ modules/relationships/app/App.js
@@ -9,7 +9,7 @@ const { __ } = wp.i18n;
 const App = () => {
 	useEffect( () => {
 		// Don't submit form when press Enter.
-		jQuery( '#post' ).on( 'keypress keydown keyup', 'input', function( e ) {
+		jQuery( '#post' ).on( 'keypress keydown keyup', 'input', function ( e ) {
 			if ( e.keyCode == 13 ) {
 				e.preventDefault();
 			}
@@ -33,6 +33,14 @@ const App = () => {
 						defaultValue={ !!MbbApp.settings.reciprocal }
 						className="relationships-plain"
 						description={ __( 'Enable only if two sides of the relationship are the same.', 'meta-box-builder' ) }
+					/>
+					<Checkbox
+						name="settings[delete_data]"
+						componentId="settings-delete-data"
+						label={ __( 'Delete data in database?' ) }
+						defaultValue={ !!MbbApp.settings.delete_data }
+						className="relationships-plain"
+						description={ __( 'Delete data in database if the relationship is deleted.', 'meta-box-builder' ) }
 					/>
 					{ sides.map( side => <Side key={ side.id } { ...side } /> ) }
 				</TabPanel>
