6fa52a393732f04feef1bbb065c7fa224be6627b
Anh Tran <anhtnt@elightup.com>
2024-11-29 14:56:19 +0700

    Quick move settings to the sidebar

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,24 +1,51 @@
 import { Button, Panel, PanelBody, PanelRow } from '@wordpress/components';
+import { Suspense, useContext } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
+import { SettingsContext } from "../contexts/SettingsContext";
+import { getControlParams } from "../functions";
+import useApi from "../hooks/useApi";
 
-const Sidebar = () => (
-	<Panel className="mb-sidebar">
-		<PanelBody title={ __( 'Summary', 'meta-box-builder' ) } initialOpen={ true }>
-			<PanelRow className="summary">
-				<p className="status"><label>{ __( 'Status', 'meta-box-builder' ) }</label> { MbbApp.status }</p>
-				<p><label>{ __( 'Published', 'meta-box-builder' ) }</label> { MbbApp.published }</p>
-				{ MbbApp.modifiedtime && <p><label>{ __( 'Last modified', 'meta-box-builder' ) }</label> { MbbApp.modifiedtime }</p> }
-				<p><label>{ __( 'Author', 'meta-box-builder' ) }</label> { MbbApp.author }</p>
-				<p><Button href={ MbbApp.trash } isDestructive={ true } variant="secondary">{ __( 'Move to trash', 'meta-box-builder' ) }</Button></p>
-			</PanelRow>
-		</PanelBody>
-		<PanelBody title={ __( 'Write a review', 'meta-box-builder' ) } initialOpen={ true }>
-			<PanelRow>
-				<p>{ __( 'If you like this plugin, please write a review on WordPress.org to help us spread the word. We really appreciate that!', 'meta-box-builder' ) }</p>
-				<p><a href="https://wordpress.org/support/plugin/meta-box/reviews/" target="_blank">{ __( 'Write a review', 'meta-box-builder' ) } &rarr;</a></p>
-			</PanelRow>
-		</PanelBody>
-	</Panel>
-);
+const Sidebar = () => {
+	const settingsControls = useApi( 'settings-controls', [] );
+	const { settings, updateSettings } = useContext( SettingsContext );
+
+	return (
+		<Panel className="mb-sidebar">
+			<PanelBody title={ __( 'Summary', 'meta-box-builder' ) } initialOpen={ true }>
+				<PanelRow className="summary">
+					<p className="status"><label>{ __( 'Status', 'meta-box-builder' ) }</label> { MbbApp.status }</p>
+					<p><label>{ __( 'Published', 'meta-box-builder' ) }</label> { MbbApp.published }</p>
+					{ MbbApp.modifiedtime && <p><label>{ __( 'Last modified', 'meta-box-builder' ) }</label> { MbbApp.modifiedtime }</p> }
+					<p><label>{ __( 'Author', 'meta-box-builder' ) }</label> { MbbApp.author }</p>
+					<p><Button href={ MbbApp.trash } isDestructive={ true } variant="secondary">{ __( 'Move to trash', 'meta-box-builder' ) }</Button></p>
+				</PanelRow>
+			</PanelBody>
+			{
+				settingsControls.length > 0 &&
+				<PanelBody title={ __( 'Settings', 'meta-box-builder' ) } initialOpen={ true }>
+					<PanelRow>
+						{
+							settingsControls.map( control => (
+								<Suspense fallback={ null } key={ control.setting }>{ getControlComponent( control, settings, updateSettings ) }</Suspense>
+							) )
+						}
+					</PanelRow>
+				</PanelBody>
+			}
+		</Panel>
+	);
+};
+
+const getControlComponent = ( control, settings, updateSettings ) => {
+	const [ Control, input, defaultValue ] = getControlParams( control, settings );
+
+	return <Control
+		componentId={ `settings-${ control.setting }` }
+		name={ `settings${ input }` }
+		{ ...control.props }
+		defaultValue={ defaultValue }
+		updateFieldData={ updateSettings }
+	/>;
+};
 
 export default Sidebar;
