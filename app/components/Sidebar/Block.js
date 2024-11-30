4de128160cc760c20d7e98b782c4d2671b68b532
Anh Tran <anhtnt@elightup.com>
2024-11-30 22:47:06 +0700

    Remove unneeded code for block settings

--- app/components/Sidebar/Block.js
+++ app/components/Sidebar/Block.js
@@ -1,5 +1,5 @@
 import { RadioControl } from "@wordpress/components";
-import { useEffect, useRef, useState } from "@wordpress/element";
+import { useEffect, useState } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
 import Icon from '../../controls/Icon';
 import Input from '../../controls/Input';
@@ -7,132 +7,18 @@ import ReactSelect from '../../controls/ReactSelect';
 import Select from '../../controls/Select';
 import Textarea from '../../controls/Textarea';
 import Toggle from "../../controls/Toggle";
-import { fetcher, getSettings } from "../../functions";
+import { getSettings } from "../../functions";
 import { ensureArray } from '/functions';
-
-const renderWithOptions = {
-	callback: __( 'PHP callback function', 'meta-box-builder' ),
-	template: __( 'Template file', 'meta-box-builder' ),
-	code: __( 'Code', 'meta-box-builder' ),
-};
-
-if ( MbbApp.extensions.views ) {
-	renderWithOptions[ 'view' ] = __( 'View', 'meta-box-builder' );
-}
-
-const getRenderViewId = ( renderView ) => {
-	const view = Object.values( MbbApp.views )?.find( view => view.post_name === renderView );
-
-	return view?.ID;
-};
 
 const Block = () => {
 	const [ settings, setSettings ] = useState( getSettings() );
 	const [ iconType, setIconType ] = useState( settings.icon_type || 'dashicons' );
-	const [ renderWith, setRenderWith ] = useState( settings.render_with || 'callback' );
-	const [ codeEditor, setCodeEditor ] = useState();
-
-	const [ views, setViews ] = useState( MbbApp.views );
-	const [ renderView, setRenderView ] = useState( settings.render_view );
-
-	const addViewButtonRef = useRef();
-	const editViewButtonRef = useRef();
-
-	const codeRef = useRef();
 
 	useEffect( () => {
 		jQuery( '.og-color-picker input[type="text"]' ).wpColorPicker();
 	}, [ iconType ] );
 
 	const updateIconType = e => setIconType( e.target.value );
-	const updateRenderWith = e => setRenderWith( e.target.value );
-
-	const [ blockPathError, setBlockPathError ] = useState( MbbApp.data?.block_path_error );
-	const [ isNewer, setIsNewer ] = useState( false );
-
-	const modalConfig = {
-		hideElement: '#editor .interface-interface-skeleton__footer, .edit-post-fullscreen-mode-close',
-		isBlockEditor: false,
-		callback: ( $modal, $modalContent ) => {
-			// Set the default type to block when adding a new view
-			$modalContent.find( '#type' ).val( 'block' );
-		},
-		closeModalCallback: ( $modal, $input ) => {
-			const postId = $modal.find( '#post_ID' ).val();
-			const postName = $modal.find( '#post_name' ).val();
-			const postTitle = $modal.find( '#title' ).val();
-
-			setViews( {
-				...views,
-				[ postId ]: { ID: postId, post_name: postName, post_title: postTitle }
-			} );
-			setRenderView( postName );
-		},
-	};
-	/**
-	 * Get local path data, including whether the path is writable, block.json version.
-	 *
-	 * @param any _
-	 * @param string path
-	 */
-	const getLocalPathData = async ( _, path ) => {
-		const postName = document.getElementById( 'post_name' ).value;
-
-		if ( !postName ) {
-			return;
-		}
-
-		const { is_writable, is_newer } = await fetcher( 'local-path-data', {
-			path,
-			version: settings.block_json?.version || 0,
-			postName
-		} );
-
-		const errorMessage = is_writable ? '' : __( 'The path is not writable.', 'meta-box-builder' );
-
-		setIsNewer( is_newer );
-		setBlockPathError( errorMessage );
-	};
-
-	const showEditViewModal = e => {
-		const $this = jQuery( e );
-
-		$this.attr( 'data-url', MbbApp.viewEditUrl + getRenderViewId( renderView ) + '&action=edit' );
-
-		$this.rwmbModal( { ...modalConfig, isEdit: true } );
-	};
-
-	const showAddViewModal = e => {
-		const $this = jQuery( e );
-
-		$this.rwmbModal( { ...modalConfig, isEdit: true } );
-	};
-
-	const handleSelectView = e => {
-		setRenderView( e.target.value );
-	};
-
-	useEffect( () => {
-		if ( !settings.block_json?.path ) {
-			return;
-		}
-
-		getLocalPathData( null, settings.block_json?.path );
-	}, [] );
-
-	useEffect( () => {
-		if ( codeEditor ) {
-			setTimeout( () => codeEditor.refresh(), 3000 );
-		}
-	}, [ codeEditor ] );
-
-	useEffect( () => {
-		showAddViewModal( addViewButtonRef?.current );
-	}, [ addViewButtonRef.current, renderWith ] );
-
-	useEffect( () => {
-		showEditViewModal( editViewButtonRef?.current );
-	}, [ editViewButtonRef.current, renderWith, renderView ] );
 
 	return <>
 		<Input
