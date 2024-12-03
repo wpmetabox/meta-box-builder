29fc9b56b680e377e2d225533a5af2098791f25f
Anh Tran <anhtnt@elightup.com>
2024-12-03 18:52:59 +0700

    Use Dropdown for icon picker

--- app/controls/Icon.js
+++ app/controls/Icon.js
@@ -1,3 +1,4 @@
+import { Dropdown } from "@wordpress/components";
 import { useState } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
 import DivRow from './DivRow';
@@ -40,48 +41,54 @@ const getIconLabel = icon => {
 };
 
 const Icon = ( { name, componentId, defaultValue, icons = MbbApp.icons, updateFieldData, ...rest } ) => {
-	const [ query, setQuery ] = useState( "" );
+	const [ query, setQuery ] = useState( '' );
 	const [ value, setValue ] = useState( defaultValue );
 	let filteredIcons = icons.map( icon => [ icon, getIconLabel( icon ) ] )
 		.filter( item => query === '' || item[ 1 ].includes( query.toLowerCase() ) );
 
-	const handleChange = e => {
-		setValue( e.target.value );
-		updateFieldData && updateFieldData( name, e.target.value );
+	const handleChange = ( icon, onToggle ) => {
+		setValue( icon );
+		updateFieldData && updateFieldData( name, icon );
+		onToggle();
 	};
 
 	return (
 		<DivRow className="og-icon" { ...rest }>
-			<div className='og-icon-selected'>
-				<span className={ `dashicons dashicons-${ value }` }></span>
-				<input
-					type="search"
-					className="og-icon-search"
-					placeholder={ __( 'Search...', 'meta-box-builder' ) }
-					value={ query }
-					onChange={ event => setQuery( event.target.value ) }
-				/>
-			</div>
-			<div className="og-icon-items">
-				{
-					filteredIcons.map( ( [ icon, label ] ) => (
-						<label key={ icon } htmlFor={ `${ componentId }-${ icon }` } className="og-icon-item" >
-							<div className="og-icon__select">
-								<input
-									id={ `${ componentId }-${ icon }` }
-									type="radio"
-									name={ name }
-									value={ icon }
-									defaultChecked={ icon === defaultValue }
-									onChange={ handleChange }
-								/>
-								<span className={ `og-dashicon dashicons dashicons-${ icon }` }></span>
-							</div>
-							<span className='og-icon-item__text'>{ label }</span>
-						</label>
-					) )
-				}
-			</div>
+			<input type="hidden" name={ name } defaultValue={ value } />
+			<Dropdown
+				popoverProps={ { placement: 'left-start' } }
+				contentClassName="og-icon__dropdown"
+				renderToggle={ ( { onToggle } ) => (
+					<button type="button" onClick={ onToggle } className="button button-secondary og-icon__pick">
+						<span className={ `dashicons dashicons-${ value }` }></span>
+					</button>
+				) }
+				renderContent={ ( { onToggle } ) => (
+					<>
+						<input
+							type="text"
+							className="og-icon__search"
+							placeholder={ __( 'Search...', 'meta-box-builder' ) }
+							value={ query }
+							onChange={ e => setQuery( e.target.value ) }
+						/>
+						<div className="og-icon__items">
+							{
+								filteredIcons.map( ( [ icon, label ] ) => (
+									<div
+										key={ icon }
+										className={ `og-icon__item ${ icon === value ? 'og-icon__item--selected' : '' }` }
+										onClick={ () => handleChange( icon, onToggle ) }
+									>
+										<span className={ `dashicons dashicons-${ icon }` }></span>
+										<div className='og-icon__text'>{ label }</div>
+									</div>
+								) )
+							}
+						</div>
+					</>
+				) }
+			/>
 		</DivRow >
 	);
 };
