b6fb6806fef6399922110c14e5c71a310f30b606
Anh Tran <anhtnt@elightup.com>
2024-12-07 09:46:48 +0700

    Put prepend & append in 1 line

--- app/controls/PrependAppend.js
+++ app/controls/PrependAppend.js
@@ -11,15 +11,13 @@ const PrependAppend = ( { defaultValue, componentId, fieldType, name, ...rest } 
 				name={ `${ name.replace( 'prepend_append', 'prepend' ) }` }
 				defaultValue={ defaultValue.prepend }
 			/>
-		</div>
-		<div className="og-input-group">
+			<label htmlFor={ `${ componentId }-append` }>{ __( 'Append', 'meta-box-builder' ) }</label>
 			<input
 				type="text"
 				id={ `${ componentId }-append` }
 				name={ `${ name.replace( 'prepend_append', 'append' ) }` }
 				defaultValue={ defaultValue.append }
 			/>
-			<label htmlFor={ `${ componentId }-append` }>{ __( 'Append', 'meta-box-builder' ) }</label>
 		</div>
 	</DivRow>
 );
