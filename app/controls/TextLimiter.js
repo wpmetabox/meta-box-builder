f483b8e466a0820e5c7db5f9e1e6e9df747d831a
Anh Tran <anhtnt@elightup.com>
2024-12-07 10:05:05 +0700

    Fix style text limit

--- app/controls/TextLimiter.js
+++ app/controls/TextLimiter.js
@@ -1,23 +1,22 @@
 import { __ } from "@wordpress/i18n";
 import DivRow from './DivRow';
 
-const TextLimiter = ( { defaultValue, componentId, fieldType, name, ...rest } ) => {
-	return [ 'text', 'textarea', 'wysiwyg' ].includes( fieldType ) &&
-		<DivRow htmlFor={ componentId } { ...rest }>
-			<div className="og-input-group">
-				<input
-					type="number"
-					min="0"
-					id={ componentId }
-					name={ `${ name }[limit]` }
-					defaultValue={ defaultValue.limit }
-				/>
-				<select name={ `${ name }[limit_type]` } defaultValue={ defaultValue.limit_type || '' }>
-					<option value="character">{ __( 'characters', 'meta-box-builder' ) }</option>
-					<option value="word">{ __( 'words', 'meta-box-builder' ) }</option>
-				</select>
-			</div>
-		</DivRow>;
-};
+const TextLimiter = ( { defaultValue, componentId, name, ...rest } ) => (
+	<DivRow className="og-text-limit" htmlFor={ componentId } { ...rest }>
+		<div className="og-input-group">
+			<input
+				type="number"
+				min="0"
+				id={ componentId }
+				name={ `${ name }[limit]` }
+				defaultValue={ defaultValue.limit }
+			/>
+			<select name={ `${ name }[limit_type]` } defaultValue={ defaultValue.limit_type || '' }>
+				<option value="character">{ __( 'characters', 'meta-box-builder' ) }</option>
+				<option value="word">{ __( 'words', 'meta-box-builder' ) }</option>
+			</select>
+		</div>
+	</DivRow>
+);
 
 export default TextLimiter;
