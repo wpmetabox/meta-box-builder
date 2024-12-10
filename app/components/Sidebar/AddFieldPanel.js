56e41c2ac2b661ba3a27014df67f441fb157a82d
Anh Tran <anhtnt@elightup.com>
2024-12-10 16:02:03 +0700

    Style add field panel

--- app/components/Sidebar/AddFieldPanel.js
+++ app/components/Sidebar/AddFieldPanel.js
@@ -1,54 +1,78 @@
-import { Panel, PanelBody } from '@wordpress/components';
+import { Button, Panel, SearchControl } from '@wordpress/components';
 import { useState } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
 import useApi from "../../hooks/useApi";
+import PersistentPanelBody from '../PersistentPanelBody';
 
 const AddFieldPanel = ( { show = true } ) => {
 	const addField = () => {};
-	const fieldCategories = useApi( 'field-categories', [] );
-	const [ searchParam, setSearchParam ] = useState( '' );
 
-	const search = e => setSearchParam( e.target.value );
-	const insert = ( e, toggle ) => {
-		addField( e.target.dataset.type );
-		toggle();
-	};
+	const [ searchQuery, setSearchQuery ] = useState( '' );
 
 	return (
 		<Panel header={ __( 'Add a new field', 'meta-box-builder' ) } className={ `mb-panel ${ show ? 'mb-panel--show' : '' }` }>
-			<PanelBody title={ __( 'Add a new field', 'meta-box-builder' ) } opened={ true }>
-				<div className="og-inserter__search">
-					<input type="search" placeholder={ __( 'Search for a field type', 'meta-box-builder' ) } onChange={ search } />
-				</div>
+			<div className='mb-panel__inner og-add-field'>
+				<SearchControl value={ searchQuery } onChange={ setSearchQuery } __nextHasNoMarginBottom />
 				{
-					fieldCategories.length > 0
-						? fieldCategories.map( category =>
-							<Category key={ category.slug } category={ category } insert={ e => insert( e, onToggle ) } searchParam={ searchParam } />
-						)
-						: <p>{ __( 'Fetching field types, please wait...', 'meta-box-builder' ) }</p>
+					searchQuery ?
+						<SearchResult searchQuery={ searchQuery } addField={ addField } />
+						: <Categories addField={ addField } />
 				}
-			</PanelBody>
+			</div>
 		</Panel>
 	);
 };
 
-const Category = ( { category, insert, searchParam } ) => {
+const Categories = ( { addField } ) => {
+	const [ activeCategory, setActiveCategory ] = useState( 'basic' );
+
+	const fieldCategories = useApi( 'field-categories', [] );
+
+	return fieldCategories.length > 0
+		? fieldCategories.map( category =>
+			<Category
+				key={ category.slug }
+				open={ category.slug === activeCategory }
+				onClick={ () => setActiveCategory( activeCategory === category.slug ? '' : category.slug ) }
+				category={ category }
+				addField={ addField }
+			/>
+		)
+		: <p>{ __( 'Fetching field types, please wait...', 'meta-box-builder' ) }</p>;
+};
+
+const Category = ( { category, addField, onClick, open } ) => {
 	const fieldTypes = useApi( 'field-types', {} );
-	const s = searchParam.toLowerCase();
-	const fields = Object.entries( fieldTypes ).filter( ( [ type, field ] ) => field.category === category.slug && field.title.toLowerCase().includes( s ) );
+	const fields = Object.entries( fieldTypes ).filter( ( [ type, field ] ) => field.category === category.slug );
 
 	return fields.length > 0 && (
-		<>
-			<div className="og-inserter__title">{ category.title }</div>
-			<div className="og-inserter__content">
-				{
-					fields.map( ( [ type, { title, description } ] ) =>
-						<div className="og-inserter__item" key={ type } data-type={ type } onClick={ insert } title={ description }>{ title }</div>
-					)
-				}
-			</div>
-		</>
+		<PersistentPanelBody title={ category.title } onClick={ onClick } open={ open }>
+			<FieldList fields={ fields } addField={ addField } />
+		</PersistentPanelBody>
 	);
 };
+
+const SearchResult = ( { searchQuery, addField } ) => {
+	const fieldTypes = useApi( 'field-types', {} );
+	const fields = Object.entries( fieldTypes ).filter( ( [ type, field ] ) => field.title.toLowerCase().includes( searchQuery.toLowerCase() ) );
+
+	return (
+		<div className='og-add-field__search-results'>
+			<FieldList fields={ fields } addField={ addField } />
+		</div>
+	);
+};
+
+const FieldList = ( { fields, addField } ) => (
+	<div className="og-add-field__list">
+		{
+			fields.map( ( [ type, field ] ) =>
+				<FieldButton key={ type } type={ type } title={ field.title } addField={ addField } />
+			)
+		}
+	</div>
+);
+
+const FieldButton = ( { type, title, addField } ) => <Button variant="secondary" onClick={ e => addField( type ) }>{ title }</Button>;
 
 export default AddFieldPanel;
