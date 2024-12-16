c20105f57928a86f8de004d378c532a940ae6862
Anh Tran <anhtnt@elightup.com>
2024-12-16 14:20:39 +0700

    Allow to toggle sidebar when clicking top buttons

--- app/components/Header.js
+++ app/components/Header.js
@@ -26,6 +26,8 @@ const Header = () => {
 		setManualSlug( true );
 	};
 
+	const updateSidebarPanel = key => () => setSidebarPanel( key === sidebarPanel ? '' : key );
+
 	return (
 		<Flex className="mb-header">
 			<Flex expanded={ false }>
@@ -63,8 +65,8 @@ const Header = () => {
 			</Flex>
 			<Flex gap={ 3 } expanded={ false } className="mb-header__actions">
 				<input type="submit" data-status="draft" className="components-button is-compact is-tertiary" value={ MbbApp.status == 'publish' ? __( 'Switch to draft', 'meta-box-builder' ) : __( 'Save draft', 'meta-box-builder' ) } />
-				<Button onClick={ () => setSidebarPanel( 'add_field' ) } className="is-compact" icon={ plusCircle } size="compact" label={ __( 'Add a new field', 'meta-box-builder' ) } showTooltip={ true } isPressed={ sidebarPanel === 'add_field' } />
-				<Button onClick={ () => setSidebarPanel( 'field_group_settings' ) } className="is-compact" icon={ drawerRight } size="compact" label={ __( 'Edit field group settings', 'meta-box-builder' ) } showTooltip={ true } isPressed={ sidebarPanel === 'field_group_settings' } />
+				<Button onClick={ updateSidebarPanel( 'add_field' ) } className="is-compact" icon={ plusCircle } size="compact" label={ __( 'Add a new field', 'meta-box-builder' ) } showTooltip={ true } isPressed={ sidebarPanel === 'add_field' } />
+				<Button onClick={ updateSidebarPanel( 'field_group_settings' ) } className="is-compact" icon={ drawerRight } size="compact" label={ __( 'Edit field group settings', 'meta-box-builder' ) } showTooltip={ true } isPressed={ sidebarPanel === 'field_group_settings' } />
 				<input type="submit" data-status="publish" className="components-button is-primary" value={ MbbApp.status == 'publish' ? __( 'Update', 'meta-box-builder' ) : __( 'Publish', 'meta-box-builder' ) } />
 			</Flex>
 		</Flex>
--- app/hooks/useSidebarPanel.js
+++ app/hooks/useSidebarPanel.js
@@ -2,7 +2,12 @@ import { create } from 'zustand';
 
 const useSidebarPanel = create( set => ( {
 	sidebarPanel: 'add_field',
-	setSidebarPanel: sidebarPanel => set( state => ( { sidebarPanel } ) ),
+	setSidebarPanel: sidebarPanel => {
+		set( state => ( { sidebarPanel } ) );
+
+		// Add an extra class to the <body> for styling.
+		document.body.classList.toggle( 'mb-no-sidebar', !sidebarPanel );
+	}
 } ) );
 
 export default useSidebarPanel;
