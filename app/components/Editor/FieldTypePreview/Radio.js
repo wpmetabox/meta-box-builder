bd2330a4a3544c8b6b7fe8406d1a18fc6db4b80f
Anh Tran <anhtnt@elightup.com>
2024-12-17 22:09:14 +0700

    Radio std is always single

--- app/components/Editor/FieldTypePreview/Radio.js
+++ app/components/Editor/FieldTypePreview/Radio.js
@@ -3,13 +3,12 @@ import { getOptions } from "../../../functions";
 const Radio = ( { field } ) => {
 	const doNothing = () => {};
 	const options = getOptions( field.options || '' );
-	const std = getOptions( field.std || '' );
 	return (
 		<fieldset className={ `rwmb-input-list ${ field.inline ? 'rwmb-inline' : '' }` }>
 			{
 				options.length > 0 && options.map( option => (
 					<label key={ option }>
-						<input type="radio" name={ `not_save[${ field._id }` } checked={ std.includes( option ) } onChange={ doNothing } />
+						<input type="radio" checked={ field.std === option } onChange={ doNothing } />
 						{ option }
 					</label>
 				) )
