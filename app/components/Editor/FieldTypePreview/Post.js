f331db6ff3086398d933a98fab5dcc6b9a0cbe2e
Anh Tran <anhtnt@elightup.com>
2024-12-23 17:04:36 +0700

    Extract object field

--- app/components/Editor/FieldTypePreview/Post.js
+++ app/components/Editor/FieldTypePreview/Post.js
@@ -1,35 +1,12 @@
-import { lazy, Suspense, useEffect, useRef } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
-import { ucwords } from "../../../functions";
+import ObjectField from "./ObjectField";
 
-const Post = ( { field } ) => {
-	const ref = useRef();
-	const type = field.field_type || 'select_advanced';
-
-	// Don't change field directly as it can fill the field settings inputs.
-	const normalizedField = {
-		...field,
-		class: `${ field.class || '' } rwmb-${ type }`,
-		placeholder: field.placeholder || __( 'Select a post', 'meta-box-builder' ),
-		options: `${ __( 'Post title 1', 'meta-box-builder' ) }\n${ __( 'Post title 2', 'meta-box-builder' ) }\n${ __( 'Post title 3', 'meta-box-builder' ) }`,
-	};
-
-	const FieldType = lazy( () => import( `./${ ucwords( type, '_', '' ) }` ) );
-
-	useEffect( () => {
-		ref.current.querySelector( '.select2-container' )?.remove();
-	}, [ type ] );
-
-	return (
-		<Suspense fallback={ null }>
-			<div ref={ ref }>
-				<FieldType field={ normalizedField } />
-				{
-					field.add_new && <a href="#" className="rwmb-modal-add-button">{ __( 'Add new', 'meta-box-builder' ) }</a>
-				}
-			</div>
-		</Suspense>
-	);
-};
+const Post = ( { field } ) => (
+	<ObjectField
+		field={ field }
+		defaultPlaceholder={ __( 'Select a post', 'meta-box-builder' ) }
+		defaultItemTitle={ __( 'Post title', 'meta-box-builder' ) }
+	/>
+);
 
 export default Post;
