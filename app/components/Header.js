a0f140cbbe288051a120d72489d8a9d8b82a8180
Anh Tran <anhtnt@elightup.com>
2024-12-26 15:43:28 +0700

    Move post_name to field group settings summary, remove logo

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,71 +1,33 @@
-import { Button, Flex, Tooltip } from '@wordpress/components';
-import { useState } from "@wordpress/element";
+import { Button, Flex } from '@wordpress/components';
 import { __ } from "@wordpress/i18n";
-import { drawerRight, plusCircle } from "@wordpress/icons";
-import AutosizeInput from 'react-input-autosize';
-import { sanitizeId } from '../functions';
+import { drawerRight, listView, plus } from "@wordpress/icons";
 import useSidebarPanel from '../hooks/useSidebarPanel';
-import { ReactComponent as Logo } from './logo.svg';
+// import { ReactComponent as Logo } from './logo.svg';
 
 const Header = () => {
 	const { sidebarPanel, setSidebarPanel } = useSidebarPanel();
-	const [ title, setTitle ] = useState( MbbApp.title );
-	const [ slug, setSlug ] = useState( MbbApp.slug );
-	const [ manualSlug, setManualSlug ] = useState( !!MbbApp.slug );
-
-	const updateTitle = e => {
-		setTitle( e.target.value );
-
-		if ( !manualSlug ) {
-			setSlug( sanitizeId( e.target.value ) );
-		}
-	};
-
-	const updateSlug = e => {
-		setSlug( e.target.value );
-		setManualSlug( true );
-	};
-
 	const updateSidebarPanel = key => () => setSidebarPanel( key === sidebarPanel ? '' : key );
 
 	return (
 		<Flex className="mb-header">
 			<Flex expanded={ false }>
-				<Tooltip delay={ 0 } text={ __( 'Back to all field groups', 'meta-box-builder' ) } placement='bottom'>
+				{/* <Tooltip delay={ 0 } text={ __( 'Back to all field groups', 'meta-box-builder' ) } placement='bottom'>
 					<a className="mb-header__logo" href={ MbbApp.url }><Logo /></a>
-				</Tooltip>
+				</Tooltip> */}
+				{/* <div className="mb-header__logo" href={ MbbApp.url }><Logo /></div> */ }
 
-				<Flex gap={ 3 } align="baseline">
-					<div className="mb-header__title" title={ __( 'Field group title', 'meta-box-builder' ) }>
-						<AutosizeInput
-							name="post_title"
-							id="post_title"
-							inputStyle={ { fontSize: 20 } }
-							value={ title }
-							onChange={ updateTitle }
-							placeholder={ __( 'Field group title', 'meta-box-builder' ) }
-						/>
-					</div>
-					<Flex gap={ 0 } align="stretch" expanded={ false } className="mb-header__badge" title={ __( 'Field group ID (slug)', 'meta-box-builder' ) }>
-						<Flex align="center" expanded={ false } className="mb-header__badge__name" as="label" htmlFor="post_name">
-							{ __( 'ID', 'meta-box-builder' ) }
-						</Flex>
-						<Flex align="center" expanded={ false } className="mb-header__badge__content">
-							<AutosizeInput
-								name="post_name"
-								id="post_name"
-								inputStyle={ { fontSize: 13 } }
-								value={ slug }
-								onChange={ updateSlug }
-								placeholder={ __( 'Field group ID', 'meta-box-builder' ) }
-							/>
-						</Flex>
-					</Flex>
-				</Flex>
+				<Button onClick={ updateSidebarPanel( 'add_field' ) } className="is-compact" icon={ plus } size="compact" variant="primary" label={ __( 'Add a new field', 'meta-box-builder' ) } showTooltip={ true } isPressed={ sidebarPanel === 'add_field' } />
+				<Button onClick={ updateSidebarPanel( 'add_field' ) } className="is-compact" icon={ listView } size="compact" label={ __( 'Show field group structure', 'meta-box-builder' ) } showTooltip={ true } isPressed={ sidebarPanel === 'add_field' } />
 			</Flex>
+			<input
+				type="text"
+				name="post_title"
+				id="post_title"
+				defaultValue={ MbbApp.title }
+				placeholder={ __( 'Field group title', 'meta-box-builder' ) }
+			/>
 			<Flex gap={ 3 } expanded={ false } className="mb-header__actions">
 				<input type="submit" data-status="draft" className="components-button is-compact is-tertiary" value={ MbbApp.status == 'publish' ? __( 'Switch to draft', 'meta-box-builder' ) : __( 'Save draft', 'meta-box-builder' ) } />
-				<Button onClick={ updateSidebarPanel( 'add_field' ) } className="is-compact" icon={ plusCircle } size="compact" label={ __( 'Add a new field', 'meta-box-builder' ) } showTooltip={ true } isPressed={ sidebarPanel === 'add_field' } />
 				<Button onClick={ updateSidebarPanel( 'field_group_settings' ) } className="is-compact" icon={ drawerRight } size="compact" label={ __( 'Edit field group settings', 'meta-box-builder' ) } showTooltip={ true } isPressed={ sidebarPanel === 'field_group_settings' } />
 				<input type="submit" data-status="publish" className="components-button is-primary" value={ MbbApp.status == 'publish' ? __( 'Update', 'meta-box-builder' ) : __( 'Publish', 'meta-box-builder' ) } />
 			</Flex>
---
+++ app/components/Sidebar/FieldGroupSettings/Summary.js
@@ -0,0 +1,18 @@
+import { Button } from '@wordpress/components';
+import { __ } from "@wordpress/i18n";
+
+const Summary = ( { show = false } ) => (
+	<>
+		<p>
+			<label htmlFor="post_name">{ __( 'ID', 'meta-box-builder' ) }</label>
+			<input type="text" name="post_name" id="post_name" defaultValue={ MbbApp.slug } />
+		</p>
+		<p className="mb-summary__status"><label>{ __( 'Status', 'meta-box-builder' ) }</label> { MbbApp.status }</p>
+		<p><label>{ __( 'Published', 'meta-box-builder' ) }</label> { MbbApp.published }</p>
+		{ MbbApp.modified && <p><label>{ __( 'Last modified', 'meta-box-builder' ) }</label> { MbbApp.modified }</p> }
+		<p><label>{ __( 'Author', 'meta-box-builder' ) }</label> { MbbApp.author }</p>
+		<p><Button href={ MbbApp.trash } isDestructive={ true } variant="secondary">{ __( 'Move to trash', 'meta-box-builder' ) }</Button></p>
+	</>
+);
+
+export default Summary;
