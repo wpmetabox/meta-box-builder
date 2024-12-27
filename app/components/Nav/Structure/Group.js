56b36455d7a66453b9f0d73019d39ecbd2d9f6bc
Anh Tran <anhtnt@elightup.com>
2024-12-27 08:22:17 +0700

    Remove field settings for structure panel

--- app/components/Nav/Structure/Group.js
+++ app/components/Nav/Structure/Group.js
@@ -1,60 +1,38 @@
 import { ReactSortable } from 'react-sortablejs';
-import useApi from "../../../hooks/useApi";
 import useLists from "../../../hooks/useLists";
-import FieldSettings from "../../Sidebar/FieldSettings/FieldSettings";
 import Node from './Node';
 
-const Group = ( { field, parent = '', updateField } ) => {
+const Group = ( { field, parent = '' } ) => {
 	const { getForList } = useLists();
 	const { fields, setFields, ...fieldActions } = getForList( field._id );
 
-	const fieldTypes = useApi( 'field-types', {} );
-
-	if ( !fieldTypes.hasOwnProperty( field.type ) ) {
-		return;
-	}
-
-	const controls = [ ...fieldTypes[ field.type ].controls ];
-
-	return (
-		<>
-			<FieldSettings
-				controls={ controls }
-				field={ field }
-				parent={ parent }
-				updateField={ updateField }
-			/>
-
-			{
-				fields.length > 0 &&
-				<div className="og-group-fields">
-					<div className="og-group-fields__inner">
-						<ReactSortable
-							group={ {
-								name: 'nested',
-								pull: true,
-								put: [ 'root', 'nested' ],
-							} }
-							animation={ 200 }
-							delayOnTouchStart={ true }
-							delay={ 2 }
-							list={ fields }
-							setList={ setFields }
-							handle=".og-item__header"
-						>
-							{
-								fields.map( f => <Node
-									key={ f._id }
-									field={ f }
-									parent={ `${ parent }[${ field._id }][fields]` }
-									{ ...fieldActions }
-								/> )
-							}
-						</ReactSortable>
-					</div>
-				</div>
-			}
-		</>
+	return fields.length > 0 && (
+		<div className="og-group-fields">
+			<div className="og-group-fields__inner">
+				<ReactSortable
+					group={ {
+						name: 'nested',
+						pull: true,
+						put: [ 'root', 'nested' ],
+					} }
+					animation={ 200 }
+					delayOnTouchStart={ true }
+					delay={ 2 }
+					list={ fields }
+					setList={ setFields }
+					handle=".og-item__header"
+				>
+					{
+						fields.map( f => <Node
+							key={ f._id }
+							field={ f }
+							parent={ `${ parent }[${ field._id }][fields]` }
+							{ ...fieldActions }
+						/> )
+					}
+				</ReactSortable>
+			</div>
+		</div>
 	);
 };
 
--- app/components/Nav/Structure/Node.js
+++ app/components/Nav/Structure/Node.js
@@ -3,18 +3,14 @@ import { memo } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
 import { isEqual } from 'lodash';
 import { getFieldIcon, inside } from "../../../functions";
-import useContextMenu from "../../../hooks/useContextMenu";
 import useFieldSettingsPanel from "../../../hooks/useFieldSettingsPanel";
 import useSidebarPanel from "../../../hooks/useSidebarPanel";
 import Actions from './Actions';
-import ContextMenu from "./ContextMenu";
-import Field from './Field';
 import Group from './Group';
 
 const Node = ( { field, parent = '', ...fieldActions } ) => {
 	const { activeField, setActiveField } = useFieldSettingsPanel();
 	const { setSidebarPanel } = useSidebarPanel();
-	const { isContextMenuOpen, openContextMenu, contextMenuPosition } = useContextMenu();
 
 	const toggleSettings = e => {
 		if ( inside( e.target, '.og-item__editable,.og-column--actions,.og-column--label,.components-menu-item__item,.og-add-field' ) ) {
@@ -25,22 +21,12 @@ const Node = ( { field, parent = '', ...fieldActions } ) => {
 		setSidebarPanel( activeField._id === field._id ? '' : 'field_settings' );
 	};
 
-	const update = ( key, value ) => {
-		if ( key.includes( '[' ) ) {
-			// Get correct key in the last [].
-			key = key.replace( /\]/g, '' ).split( '[' ).pop();
-		}
-
-		fieldActions.updateField( field._id, key, value );
-	};
-
 	return field.type && (
 		<div className={ `og-item og-item--${ field.type } ${ field._id === activeField._id ? 'og-item--active' : '' }` }>
 			<div
 				className="og-item__header"
 				title={ __( 'Click to reveal field settings. Drag and drop to reorder fields.', 'meta-box-builder' ) }
 				onClick={ toggleSettings }
-				onContextMenu={ openContextMenu }
 			>
 				<span className="og-column--label">
 					<Icon icon={ getFieldIcon( field.type ) } />
@@ -52,18 +38,7 @@ const Node = ( { field, parent = '', ...fieldActions } ) => {
 				</span>
 			</div>
 			{
-				<ContextMenu
-					open={ isContextMenuOpen }
-					top={ contextMenuPosition.y }
-					left={ contextMenuPosition.x }
-					field={ field }
-					{ ...fieldActions }
-				/>
-			}
-			{
-				field.type === 'group'
-					? <Group field={ field } parent={ parent } updateField={ update } />
-					: <Field field={ field } parent={ parent } updateField={ update } />
+				field.type === 'group' && <Group field={ field } parent={ parent } />
 			}
 		</div>
 	);
