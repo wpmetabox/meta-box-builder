54681712f4170a26b76f7925162f705a23cce28b
Tan Nguyen <hi@tan.ng>
2025-01-03 04:00:24 +0000

    more invert methods

--- vendor/meta-box/mbb-parser/src/Parsers/Field.php
+++ vendor/meta-box/mbb-parser/src/Parsers/Field.php
@@ -6,16 +6,16 @@ class Field extends Base {
 	protected $empty_keys = [ 'save_field' ];
 
 	// Remove if "true", set to "false" if missing.
-	protected $default_true = [
-		'button_group'   => [ 'inline' ],
-		'radio'          => [ 'inline' ],
-		'file_advanced'  => [ 'max_status' ],
-		'file_upload'    => [ 'max_status' ],
+	protected $default_true = [ 
+		'button_group' => [ 'inline' ],
+		'radio' => [ 'inline' ],
+		'file_advanced' => [ 'max_status' ],
+		'file_upload' => [ 'max_status' ],
 		'image_advanced' => [ 'max_status' ],
-		'image_upload'   => [ 'max_status' ],
-		'video'          => [ 'max_status' ],
+		'image_upload' => [ 'max_status' ],
+		'video' => [ 'max_status' ],
 	];
-	private $choice_types   = [ 'select', 'radio', 'checkbox_list', 'select_advanced', 'button_group', 'image_select', 'autocomplete' ];
+	private $choice_types = [ 'select', 'radio', 'checkbox_list', 'select_advanced', 'button_group', 'image_select', 'autocomplete' ];
 
 	public function parse() {
 		// Remove collapse/expand state.
@@ -78,8 +78,8 @@ class Field extends Base {
 		if ( empty( $this->settings['datalist_choices'] ) ) {
 			return $this;
 		}
-		$this->datalist = [
-			'id'      => uniqid(),
+		$this->datalist = [ 
+			'id' => uniqid(),
 			'options' => explode( "\n", $this->settings['datalist_choices'] ),
 		];
 		unset( $this->settings['datalist_choices'] );
@@ -128,6 +128,15 @@ class Field extends Base {
 		return $this;
 	}
 
+	private function invert_object_field() {
+		if ( ! in_array( $this->type, [ 'taxonomy', 'taxonomy_advanced', 'post', 'user' ], true ) ) {
+			return $this;
+		}
+
+		// @todo set terms, inline, multiple, select_all_none
+		return $this;
+	}
+
 	private function parse_choice_options() {
 		if ( ! in_array( $this->type, $this->choice_types ) ) {
 			return $this;
@@ -142,7 +151,7 @@ class Field extends Base {
 			if ( is_callable( $callback ) ) {
 				try {
 					$this->options = call_user_func( $callback );
-				} catch ( \Throwable $th ) {
+				} catch (\Throwable $th) {
 					// throw $th;
 				}
 				$this->_callback = $callback; // For using in the encoders.
@@ -216,6 +225,16 @@ class Field extends Base {
 		return $this;
 	}
 
+	private function invert_choice_std() {
+		if ( ! in_array( $this->type, $this->choice_types ) ) {
+			return $this;
+		}
+
+		$this->std = is_array( $this->std ) ? implode( "\r\n", $this->std ) : $this->std;
+
+		return $this;
+	}
+
 	private function parse_clone() {
 		if ( $this->clone ) {
 			return $this;
@@ -264,6 +283,12 @@ class Field extends Base {
 		return $this;
 	}
 
+	private function invert_field_key_value() {
+		// @todo: implement this
+
+		return $this;
+	}
+
 	private function parse_field_group() {
 		$this->remove_default( 'default_state', 'expanded' );
 		if ( $this->collapsible ) {
@@ -273,6 +298,12 @@ class Field extends Base {
 		foreach ( $keys as $key ) {
 			unset( $this->$key );
 		}
+		return $this;
+	}
+
+	private function invert_field_group() {
+		// @todo: Implement this and test
+
 		return $this;
 	}
 
@@ -287,6 +318,19 @@ class Field extends Base {
 		}
 
 		unset( $this->text_limiter );
+		return $this;
+	}
+
+	private function invert_text_limiter() {
+		if ( ! isset( $this->limit ) ) {
+			return $this;
+		}
+
+		$this->text_limiter = [ 
+			'limit' => $this->limit,
+			'limit_type' => $this->limit_type ?? 'word',
+		];
+
 		return $this;
 	}
 }
