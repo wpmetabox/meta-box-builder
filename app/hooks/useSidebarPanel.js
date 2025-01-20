4e5785009c280f60a88b471a51b3e538400ffc49
Anh Tran <anhtnt@elightup.com>
2025-01-20 17:05:32 +0700

    Do not update sidebar when no changes

--- app/hooks/useSidebarPanel.js
+++ app/hooks/useSidebarPanel.js
@@ -1,8 +1,12 @@
 import { create } from 'zustand';
 
-const useSidebarPanel = create( set => ( {
+const useSidebarPanel = create( ( set, get ) => ( {
 	sidebarPanel: 'field_group_settings',
-	setSidebarPanel: sidebarPanel => set( state => ( { sidebarPanel } ) ),
+	setSidebarPanel: sidebarPanel => {
+		if ( get().sidebarPanel !== sidebarPanel ) {
+			set( state => ( { sidebarPanel } ) );
+		}
+	},
 } ) );
 
 export default useSidebarPanel;
