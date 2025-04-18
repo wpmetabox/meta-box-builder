53fc3c8528f6858704fa20c9273673742c474477
Anh Tran <anhtnt@elightup.com>
2025-04-18 21:16:18 +0700

    Remove dead code

--- src/Integrations/Polylang/FieldGroupValues.php
+++ src/Integrations/Polylang/FieldGroupValues.php
@@ -2,23 +2,12 @@
 namespace MBB\Integrations\Polylang;
 
 use MBB\Control;
-use RW_Meta_Box;
 
 class FieldGroupValues {
-	// Define test field IDs as constants
-	const TEST_FIELD_IDS = [
-		'text_field',
-		'textarea_field',
-		'wysiwyg_field'
-	];
-
 	const MODES = [ 'translate', 'copy', 'ignore', 'advanced' ];
 
 	public function __construct() {
 		add_filter( 'mbb_settings_controls', [ $this, 'add_translation_control' ] );
-
-		// Add filter to handle field value translations
-		// add_filter( 'rwmb_field_meta', [ $this, 'translate_field_value' ], 10, 4 );
 
 		// Tell Polylang whether to translate or copy a field
 		add_filter( 'pll_copy_post_metas', [ $this, 'copy_post_metas' ], 10, 3 );
@@ -32,7 +21,7 @@ class FieldGroupValues {
 			'options' => [
 				'ignore'    => __( 'Do not translate any fields in this field group', 'meta-box-builder' ),
 				'translate' => __( 'Translate all fields in this field group', 'meta-box-builder' ),
-				'copy'      => __( 'Synchronize values accross languages', 'meta-box-builder' ),
+				'copy'      => __( 'Synchronize values across languages', 'meta-box-builder' ),
 				'advanced'  => __( 'Set translation mode per field', 'meta-box-builder' ),
 			],
 		], 'ignore' );
@@ -80,89 +69,5 @@ class FieldGroupValues {
 		}
 
 		return $fields;
-	}
-
-	/**
-	 * Translate field value based on the translation mode.
-	 *
-	 * @param mixed  $meta   Field meta value.
-	 * @param array  $args   Additional arguments for getting meta value.
-	 * @param array  $field  Field parameters.
-	 * @param object $object Current object being processed.
-	 * @return mixed Translated meta value.
-	 */
-	public function translate_field_value( $meta, $args, $field, $object ) {
-		// For testing, only process our test fields
-		if ( ! in_array( $field['id'], self::TEST_FIELD_IDS ) ) {
-			return $meta;
-		}
-
-		// Get current language and post language
-		$current_language = pll_current_language();
-		$post_language = isset( $object->ID ) ? pll_get_post_language( $object->ID ) : '';
-
-		// If languages match, no translation needed
-		if ( $current_language === $post_language || empty( $current_language ) || empty( $post_language ) ) {
-			return $meta;
-		}
-
-		// Get field group settings
-		$field_group_id = $field['field_group'] ?? 0;
-		if ( ! $field_group_id ) {
-			return $meta;
-		}
-
-		// Get translation mode from field group settings
-		$translation_mode = $this->get_field_group_translation_mode( $field_group_id );
-
-		// Handle based on translation mode
-		switch ( $translation_mode ) {
-			case 'translate':
-				// Get translated value from the translated post
-				$translated_post_id = $this->get_translated_post_id( $object->ID, $current_language );
-				if ( $translated_post_id ) {
-					$translated_meta = get_post_meta( $translated_post_id, $field['id'], true );
-					if ( ! empty( $translated_meta ) ) {
-						return $translated_meta;
-					}
-				}
-				break;
-
-			case 'copy':
-				// Keep the same value across languages
-				return $meta;
-
-			case 'advanced':
-				// Get field-specific translation mode
-				$field_translation = $this->get_field_translation_mode( $field_group_id, $field['id'] );
-				if ( $field_translation === 'translate' ) {
-					$translated_post_id = $this->get_translated_post_id( $object->ID, $current_language );
-					if ( $translated_post_id ) {
-						$translated_meta = get_post_meta( $translated_post_id, $field['id'], true );
-						if ( ! empty( $translated_meta ) ) {
-							return $translated_meta;
-						}
-					}
-				}
-				break;
-
-			case 'ignore':
-			default:
-				// Do nothing, return original value
-				break;
-		}
-
-		return $meta;
-	}
-
-	/**
-	 * Get translated post ID.
-	 *
-	 * @param int    $post_id  Original post ID.
-	 * @param string $language Target language.
-	 * @return int|false Translated post ID or false.
-	 */
-	private function get_translated_post_id( $post_id, $language ) {
-		return pll_get_post( $post_id, $language );
 	}
 }
