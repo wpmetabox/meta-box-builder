0540ec8f8c19acde82e0740291b8f31200c044cc
Anh Tran <anhtnt@elightup.com>
2025-05-09 16:55:17 +0700

    Add built files

--- assets/js/build/app.asset.php
+++ assets/js/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'df05b2688ea14e1ae34b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c0f0235f978d18530214');
