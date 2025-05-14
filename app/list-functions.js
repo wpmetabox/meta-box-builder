da809fb8d404c4a4365547e8e8cdbf3988e926a7
Anh Tran <anhtnt@elightup.com>
2025-05-14 09:01:46 +0700

    Add init parse lists

--- app/list-functions.js
+++ app/list-functions.js
@@ -1,6 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { create } from 'zustand';
-import { getFieldValue, ucwords, uniqid } from './functions';
+import { ensureArray, getFieldValue, ucwords, uniqid } from './functions';
 import useNavPanel from './hooks/useNavPanel';
 
 const areFieldsEqual = ( a, b ) => a.length === b.length && a.every( ( field, index ) => field._id === b[ index ]._id );
@@ -16,170 +16,205 @@ const createNewField = type => {
 	};
 };
 
-const createList = create( ( set, get ) => ( {
-	id: '',
-	fields: [],
-	baseInputName: '',
+const lists = new Map();
 
-	setListInfo: ( id, baseInputName ) => set( { id, baseInputName } ),
-	addFieldAt: ( fieldType, position ) => {
-		const { fields, baseInputName } = get();
-
-		if ( position < 0 || position >= fields.length ) {
-			console.error( 'Invalid position.' );
-			return;
-		}
-
-		const newField = createNewField( fieldType );
-
-		set( state => {
-			// Add field to the list
-			const newFields = [
-				...state.fields.slice( 0, position ),
-				newField,
-				...state.fields.slice( position )
-			];
-
-			return { fields: newFields };
-		} );
-	},
-	addField: ( fieldType ) => {
-		const newField = createNewField( fieldType );
-
-		set( state => ( {
-			fields: [ ...state.fields, newField ]
-		} ) );
-	},
-	prependField: ( fieldType ) => {
-		const newField = createNewField( fieldType );
-
-		set( state => ( {
-			fields: [ newField, ...state.fields ]
-		} ) );
-	},
-	addFieldBefore: ( fieldId, fieldType ) => {
-		const newField = createNewField( fieldType );
-
-		set( state => {
-			// Find the index of the field
-			const index = state.fields.findIndex( f => f._id === fieldId );
-			if ( index === -1 ) {
-				console.error( `Field with id ${ fieldId } not found.` );
-				return state;
-			}
-
-			let newFields = [ ...state.fields ];
-			newFields.splice( index, 0, newField );
-
-			return { fields: newFields };
-		} );
-	},
-	addFieldAfter: ( fieldId, fieldType ) => {
-		const newField = createNewField( fieldType );
-
-		set( state => {
-			// Find the index of the field
-			const index = state.fields.findIndex( f => f._id === fieldId );
-			if ( index === -1 ) {
-				console.error( `Field with id ${ fieldId } not found.` );
-				return state;
-			}
-
-			let newFields = [ ...state.fields ];
-			newFields.splice( index + 1, 0, newField );
-
-			return { fields: newFields };
-		} );
-	},
-	duplicateField: ( fieldId ) => {
-		const { baseInputName } = get();
-
-		let newField = getFieldValue( `${ baseInputName }[${ fieldId }]` );
-		const newId = `${ newField.type }_${ uniqid() }`;
-
-		newField.id = newId;
-		newField._id = newId;
-		newField.name += __( ' (Copy)', 'meta-box-builder' );
-
-		set( state => {
-			// Find the index of the field
-			const index = state.fields.findIndex( f => f._id === fieldId );
-			if ( index === -1 ) {
-				console.error( `Field with id ${ fieldId } not found.` );
-				return state;
-			}
-
-			let newFields = [ ...state.fields ];
-			newFields.splice( index + 1, 0, newField );
-
-			return { fields: newFields };
-		} );
-	},
-	removeField: ( fieldId ) => {
-		const { navPanel, setNavPanel } = useNavPanel.getState();
-
-		set( state => ( {
-			fields: state.fields.filter( f => f._id !== fieldId )
-		} ) );
-
-		if ( navPanel === 'field-settings' ) {
-			setNavPanel( '' );
-		}
-	},
-	updateField: ( fieldId, key, value ) => {
-		const field = get().fields.find( f => f._id === fieldId );
-		if ( !field ) {
-			console.error( `Field with id ${ fieldId } not found.` );
-			return;
-		}
-
-		// Don't update if the value is the same.
-		if ( field[ key ] === value ) {
-			return;
-		}
-
-		set( state => ( {
-			fields: state.fields.map( f =>
-				f._id === fieldId ? { ...f, [ key ]: value } : f
-			)
-		} ) );
-	},
-	moveFieldUp: ( fieldId ) => set( state => {
-		const index = state.fields.findIndex( f => f._id === fieldId );
-		if ( index <= 0 ) {
-			return state;
-		}
-
-		let newFields = [ ...state.fields ];
-		[ newFields[ index - 1 ], newFields[ index ] ] = [ newFields[ index ], newFields[ index - 1 ] ];
-
-		return { fields: newFields };
-	} ),
-	moveFieldDown: ( fieldId ) => set( state => {
-		const index = state.fields.findIndex( f => f._id === fieldId );
-		if ( index >= state.fields.length - 1 ) {
-			return state;
-		}
-
-		let newFields = [ ...state.fields ];
-		[ newFields[ index ], newFields[ index + 1 ] ] = [ newFields[ index + 1 ], newFields[ index ] ];
-
-		return { fields: newFields };
-	} ),
-	setFields: ( fields ) => {
-		if ( areFieldsEqual( get().fields, fields ) ) {
-			return;
-		}
-
-		set( { fields } );
-	},
-} ) );
-
-const lists = new Map(); // or a proxy if needed
-
-export function getList( id ) {
-	if ( !lists.has( id ) ) {
-		lists.set( id, createList() );
+const createList = ( { id = '', fields = [], baseInputName = '' } ) => {
+	if ( lists.has( id ) ) {
+		return lists.get( id );
 	}
-	return lists.get( id );
-}
+
+	const list = create( ( set, get ) => ( {
+		id,
+		fields,
+		baseInputName,
+
+		addFieldAt: ( fieldType, position ) => {
+			const { fields, baseInputName } = get();
+
+			if ( position < 0 || position >= fields.length ) {
+				console.error( 'Invalid position.' );
+				return;
+			}
+
+			const newField = createNewField( fieldType );
+
+			set( state => {
+				// Add field to the list
+				const newFields = [
+					...state.fields.slice( 0, position ),
+					newField,
+					...state.fields.slice( position )
+				];
+
+				return { fields: newFields };
+			} );
+
+			// Create a new list for group fields.
+			if ( fieldType === 'group' ) {
+				createList( {
+					id: newField._id,
+					fields: [],
+					baseInputName: `${ baseInputName }[${ newField._id }][fields]`,
+				} );
+			}
+		},
+		addField: ( fieldType ) => {
+			const newField = createNewField( fieldType );
+
+			set( state => ( {
+				fields: [ ...state.fields, newField ]
+			} ) );
+		},
+		prependField: ( fieldType ) => {
+			const newField = createNewField( fieldType );
+
+			set( state => ( {
+				fields: [ newField, ...state.fields ]
+			} ) );
+		},
+		addFieldBefore: ( fieldId, fieldType ) => {
+			const newField = createNewField( fieldType );
+
+			set( state => {
+				// Find the index of the field
+				const index = state.fields.findIndex( f => f._id === fieldId );
+				if ( index === -1 ) {
+					console.error( `Field with id ${ fieldId } not found.` );
+					return state;
+				}
+
+				let newFields = [ ...state.fields ];
+				newFields.splice( index, 0, newField );
+
+				return { fields: newFields };
+			} );
+		},
+		addFieldAfter: ( fieldId, fieldType ) => {
+			const newField = createNewField( fieldType );
+
+			set( state => {
+				// Find the index of the field
+				const index = state.fields.findIndex( f => f._id === fieldId );
+				if ( index === -1 ) {
+					console.error( `Field with id ${ fieldId } not found.` );
+					return state;
+				}
+
+				let newFields = [ ...state.fields ];
+				newFields.splice( index + 1, 0, newField );
+
+				return { fields: newFields };
+			} );
+		},
+		duplicateField: ( fieldId ) => {
+			const { baseInputName } = get();
+
+			let newField = getFieldValue( `${ baseInputName }[${ fieldId }]` );
+			const newId = `${ newField.type }_${ uniqid() }`;
+
+			newField.id = newId;
+			newField._id = newId;
+			newField.name += __( ' (Copy)', 'meta-box-builder' );
+
+			set( state => {
+				// Find the index of the field
+				const index = state.fields.findIndex( f => f._id === fieldId );
+				if ( index === -1 ) {
+					console.error( `Field with id ${ fieldId } not found.` );
+					return state;
+				}
+
+				let newFields = [ ...state.fields ];
+				newFields.splice( index + 1, 0, newField );
+
+				return { fields: newFields };
+			} );
+		},
+		removeField: ( fieldId ) => {
+			const { navPanel, setNavPanel } = useNavPanel.getState();
+
+			set( state => ( {
+				fields: state.fields.filter( f => f._id !== fieldId )
+			} ) );
+
+			if ( navPanel === 'field-settings' ) {
+				setNavPanel( '' );
+			}
+		},
+		updateField: ( fieldId, key, value ) => {
+			const field = get().fields.find( f => f._id === fieldId );
+			if ( !field ) {
+				console.error( `Field with id ${ fieldId } not found.` );
+				return;
+			}
+
+			// Don't update if the value is the same.
+			if ( field[ key ] === value ) {
+				return;
+			}
+
+			set( state => ( {
+				fields: state.fields.map( f =>
+					f._id === fieldId ? { ...f, [ key ]: value } : f
+				)
+			} ) );
+		},
+		moveFieldUp: ( fieldId ) => set( state => {
+			const index = state.fields.findIndex( f => f._id === fieldId );
+			if ( index <= 0 ) {
+				return state;
+			}
+
+			let newFields = [ ...state.fields ];
+			[ newFields[ index - 1 ], newFields[ index ] ] = [ newFields[ index ], newFields[ index - 1 ] ];
+
+			return { fields: newFields };
+		} ),
+		moveFieldDown: ( fieldId ) => set( state => {
+			const index = state.fields.findIndex( f => f._id === fieldId );
+			if ( index >= state.fields.length - 1 ) {
+				return state;
+			}
+
+			let newFields = [ ...state.fields ];
+			[ newFields[ index ], newFields[ index + 1 ] ] = [ newFields[ index + 1 ], newFields[ index ] ];
+
+			return { fields: newFields };
+		} ),
+		setFields: ( fields ) => {
+			if ( areFieldsEqual( get().fields, fields ) ) {
+				return;
+			}
+
+			set( { fields } );
+		},
+	} ) );
+
+	lists.set( id, list );
+	return list;
+};
+
+const getList = id => {
+	if ( lists.has( id ) ) {
+		return lists.get( id );
+	}
+	console.error( `%cList with id "${ id }" not found.`, 'color:red' );
+};
+
+// Parse fields and put into the lists.
+// Recursively put groups' fields into other lists.
+const parseLists = ( obj, listId, baseInputName ) => {
+	let fields = ensureArray( obj.fields );
+	fields = fields.filter( field => field.type );
+
+	createList( { id: listId, fields, baseInputName } );
+
+	fields.forEach( field => {
+		if ( field.type === 'group' ) {
+			parseLists( field, field._id, `${ baseInputName }[${ field._id }][fields]` );
+		}
+	} );
+};
+parseLists( MbbApp, 'root', 'fields' );
+
+export default getList;
