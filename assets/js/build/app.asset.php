e4352962137fbd31566e3839c73ce80c0bf58dc5
Anh Tran <anhtnt@elightup.com>
2025-05-15 14:26:32 +0700

    Update built files

--- assets/js/build/app.asset.php
+++ assets/js/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'fb32f3f26d026efa538c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '922b4ee951f073d1617d');
