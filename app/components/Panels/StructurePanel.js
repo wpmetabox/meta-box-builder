438d7610fb40df86f2ba8888fd0bf3a4dc6a3b4c
Anh Tran <anhtnt@elightup.com>
2025-06-16 17:15:21 +0700

    Add show as a floating panel button

--- app/components/Panels/StructurePanel.js
+++ app/components/Panels/StructurePanel.js
@@ -1,9 +1,22 @@
-import { Panel } from '@wordpress/components';
+import { Button, Flex, Panel } from '@wordpress/components';
 import { __ } from "@wordpress/i18n";
+import { external } from '@wordpress/icons';
 import Fields from './Structure/Fields';
 
+const Header = () => (
+	<Flex align="center" justify="space-between" gap={ 1 }>
+		{ __( 'Structure', 'meta-box-builder' ) }
+		<Button
+			icon={ external }
+			iconSize={ 16 }
+			label={ __( 'Show as a floating panel', 'meta-box-builder' ) }
+			showTooltip={ true }
+		/>
+	</Flex>
+);
+
 const StructurePanel = () => (
-	<Panel header={ __( 'Structure', 'meta-box-builder' ) } className="mb-panel mb-panel--structure">
+	<Panel header={ <Header /> } className="mb-panel mb-panel--structure">
 		<div className="mb-panel__inner">
 			<Fields />
 		</div>
