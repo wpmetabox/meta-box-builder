37b6e2eea520d194ef17c3bc58c6fb3280ed7c01
Anh Tran <anhtnt@elightup.com>
2025-07-02 11:43:11 +0700

    Update SelectAdvanced

--- app/components/Editor/FieldTypePreview/SelectAdvanced.js
+++ app/components/Editor/FieldTypePreview/SelectAdvanced.js
@@ -1,11 +1,12 @@
 import { useEffect, useRef } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
-import { doNothing, getOptions } from "../../../functions";
+import { arrayUniqueByKey, doNothing, getFullOptions } from "../../../functions";
 
 const SelectAdvanced = ( { field } ) => {
-	const options = getOptions( field.options || '' );
-	let std = field.std || '';
-	std = field.multiple ? getOptions( std ) : std;
+	const options = arrayUniqueByKey( getFullOptions( field.options || '' ), 'value' );
+	let std = getFullOptions( field.std || '' ).map( option => option.value );
+	std = field.multiple ? std : std[ 0 ];
+
 	const ref = useRef();
 
 	useEffect( () => {
@@ -26,7 +27,7 @@ const SelectAdvanced = ( { field } ) => {
 		<>
 			<select multiple={ field.multiple } ref={ ref } value={ std } onChange={ doNothing }>
 				<option value="">{ field.placeholder || __( 'Select an item', 'meta-box-builder' ) }</option>
-				{ options.map( option => <option key={ option } value={ option }>{ option }</option> ) }
+				{ options.map( option => <option key={ option.value } value={ option.value }>{ option.label }</option> ) }
 			</select>
 			{
 				field.multiple && field.select_all_none &&
