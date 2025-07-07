09407b71ccf8a1b2e83fc697354ed7b1722a8225
Anh Tran <anhtnt@elightup.com>
2025-07-07 11:19:53 +0700

    Build for other modules

--- src/Extensions/Relationships/build/relationships.asset.php
+++ src/Extensions/Relationships/build/relationships.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '6e036a47d03cc2d3b8b6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '14a7da7f386a69dc5e23');
--- src/Extensions/SettingsPage/build/settings-page.asset.php
+++ src/Extensions/SettingsPage/build/settings-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'ca9f579e4acbb7cbb833');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '2709fd6fe01cda191f37');
