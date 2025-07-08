5f99df1785798b7b1ff3dffdefe5adbf6d925005
Anh Tran <anhtnt@elightup.com>
2025-07-08 21:56:34 +0700

    Show description instead of tooltip for advanced field group settings

--- app/components/Panels/FieldGroupSettings/Advanced.js
+++ app/components/Panels/FieldGroupSettings/Advanced.js
@@ -12,7 +12,7 @@ const Advanced = () => {
 			<Input
 				name="class"
 				label={ __( 'Custom CSS class', 'meta-box-builder' ) }
-				tooltip={ __( 'Custom CSS class for the wrapper div', 'meta-box-builder' ) }
+				description={ __( 'Custom CSS class for the wrapper div.', 'meta-box-builder' ) }
 				componentId="settings-class"
 				defaultValue={ getSetting( 'class', '' ) }
 				updateField={ updateSetting }
@@ -20,7 +20,7 @@ const Advanced = () => {
 			<DivRow
 				htmlFor="settings-prefix"
 				label={ __( 'Field ID prefix', 'meta-box-builder' ) }
-				tooltip={ __( 'Auto add a prefix to all field IDs to keep them separated from other field groups or other plugins. Leave empty to ignore this or use underscore (_) to make the fields hidden from the default WordPress\'s Custom Fields meta box.', 'meta-box-builder' ) }
+				description={ __( 'Add a prefix to all field IDs to keep them separated from other field groups or other plugins.', 'meta-box-builder' ) }
 			>
 				<input
 					type="text"
@@ -30,11 +30,10 @@ const Advanced = () => {
 					onChange={ e => updatePrefix( e.target.value ) }
 				/>
 			</DivRow>
-
 			<KeyValue
 				name="custom_settings"
 				label={ __( 'Custom settings', 'meta-box-builder' ) }
-				tooltip={ __( 'Apply to the current field group. For individual fields, please go to each field > tab Advanced.', 'meta-box-builder' ) }
+				description={ __( 'Add custom settings to the field. Will overwrite existing settings if they have the <a href="https://docs.metabox.io/creating-fields-with-code/#field-group-settings" target="_blank">same key</a>. Use <a href="https://docs.metabox.io/extensions/meta-box-builder/#custom-settings" target="_blank">dot/JSON notation</a> to add nested settings.', 'meta-box-builder' ) }
 				componentId="settings-custom_settings"
 				defaultValue={ getSetting( 'custom_settings', {} ) }
 				updateField={ updateSetting }
