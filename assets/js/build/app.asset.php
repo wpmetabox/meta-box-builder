f2b0092953d949f4bd690210476b721483c35b68
Anh Tran <anhtnt@elightup.com>
2025-07-08 10:49:53 +0700

    Build

--- assets/js/build/app.asset.php
+++ assets/js/build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '63862ecfd93ed49b8086');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '58b66959035cbf21478b');
