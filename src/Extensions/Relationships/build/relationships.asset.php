8276bcd3ebaf864a11b0efbf2e794c396e93ea3e
Anh Tran <anhtnt@elightup.com>
2025-07-11 16:03:43 +0700

    Build relationships & settings page

--- src/Extensions/Relationships/build/relationships.asset.php
+++ src/Extensions/Relationships/build/relationships.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '2e3e9e83bafc29bb7b07');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '2ad56db65cdaa1f48b2b');
--- src/Extensions/SettingsPage/build/settings-page.asset.php
+++ src/Extensions/SettingsPage/build/settings-page.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '816c0f8c1cbd04c751af');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'f6ba551fd7ff59cfb5ec');
