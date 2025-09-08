7d1e41002e2d91cf7f933102695be4953cc807fa
Anh Tran <anhtnt@elightup.com>
2025-09-08 15:14:15 +0700

    Use icon for required & cloneable settings for better accessibility

--- assets/app/controls/Required.js
+++ assets/app/controls/Required.js
@@ -1,6 +1,6 @@
-const Required = ( { name, label, defaultValue, updateField, ...rest } ) => (
+const Required = ( { name, label, defaultValue, updateField } ) => (
 	<label className="og-status">
-		<input type="checkbox" name={ name } checked={ defaultValue } value={ true } onChange={ e => updateField( name, e.target.checked ) } />
+		<input type="checkbox" checked={ defaultValue } value={ true } onChange={ e => updateField( name, e.target.checked ) } />
 		{ label }
 	</label>
 );
