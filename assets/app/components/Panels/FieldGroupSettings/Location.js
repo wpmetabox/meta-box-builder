8eb4bf7fe12258850f5b984a672900332ad1e9f3
Anh Tran <anhtnt@elightup.com>
2025-09-08 14:57:36 +0700

    Fix compatible with Gutenberg plugin
    https://app.asana.com/1/11768486970802/project/17125277541504/task/1211281518110483?focus=true
    https://support.metabox.io/topic/error-when-gutenberg-plugin-activated

--- assets/app/components/Panels/FieldGroupSettings/Location.js
+++ assets/app/components/Panels/FieldGroupSettings/Location.js
@@ -1,4 +1,4 @@
-import { __ } from "@wordpress/i18n";
+import { __, sprintf } from "@wordpress/i18n";
 import DivRow from '../../../controls/DivRow';
 import ReactSelect from '../../../controls/ReactSelect';
 import Select from '../../../controls/Select';
