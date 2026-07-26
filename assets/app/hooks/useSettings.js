3201aefae329e7b9fd496e128eeb41dd6ac81762
LyNguyen <cnttnt@gmail.com>
2026-07-26 08:40:41 +0700

    Update order

--- assets/app/hooks/useSettings.js
+++ assets/app/hooks/useSettings.js
@@ -78,7 +78,7 @@ const useSettings = create( ( set, get ) => ( {
 			dotProp.delete( updatedSettings, 'post_types' );
 			dotProp.delete( updatedSettings, 'taxonomies' );
 			dotProp.delete( updatedSettings, 'type' );
-		} else if ( [ 'block', 'user', 'comment' ].includes( value ) ) {
+		} else if ( [ 'block', 'user', 'comment', 'order' ].includes( value ) ) {
 			dotProp.delete( updatedSettings, 'post_types' );
 			dotProp.delete( updatedSettings, 'taxonomies' );
 			dotProp.delete( updatedSettings, 'settings_pages' );
